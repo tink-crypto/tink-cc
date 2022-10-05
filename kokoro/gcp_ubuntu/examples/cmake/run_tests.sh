e4958213f31cc4b9791968b37b0f60474b7a7187
Moreno Ambrosin <ambrosin@google.com>
2022-10-05 04:48:20 -0700

    Add Kokoro test script to build examples with CMake.
    PiperOrigin-RevId: 479013741
    Change-Id: Ic7e83fbe5973988ce0b1f62e3abfb653db06d2cc

---
+++ kokoro/gcp_ubuntu/examples/cmake/run_tests.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+set -euo pipefail
+
+# If we are running on Kokoro cd into the repository.
+if [[ -n "${KOKORO_ARTIFACTS_DIR:-}" ]]; then
+  TINK_BASE_DIR="$(echo "${KOKORO_ARTIFACTS_DIR}"/git*)"
+  cd "${TINK_BASE_DIR}/tink_cc"
+fi
+
+: "${TINK_BASE_DIR:=$(cd .. && pwd)}"
+
+# Sourcing required to update callers environment.
+source ./kokoro/testutils/install_python3.sh
+
+# TODO(b/251078186): Test helloworld too.
+./kokoro/testutils/run_cmake_tests.sh "examples/walkthrough" \
+  -DTINK_BUILD_TESTS=OFF

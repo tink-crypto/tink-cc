d948dfa03b3e605cd39150ce3de9a8ccd6a83f2c
Moreno Ambrosin <ambrosin@google.com>
2022-11-04 05:34:46 -0700

    Avoid installing OpenSSL during CMake OpenSSL tests when running Kokoro GCP Ubuntu VMs.
    PiperOrigin-RevId: 486118409
    Change-Id: I499657892ea358ff07bff682b16747cf7a61b254

--- kokoro/gcp_ubuntu/cmake_openssl/run_tests.sh
+++ kokoro/gcp_ubuntu/cmake_openssl/run_tests.sh
@@ -22,6 +22,4 @@ if [[ -n "${KOKORO_ARTIFACTS_DIR:-}" ]]; then
 fi
 
 ./kokoro/testutils/update_certs.sh
-# Sourcing is needed to update the caller environment.
-source ./kokoro/testutils/install_openssl.sh
 ./kokoro/testutils/run_cmake_tests.sh . -DTINK_USE_SYSTEM_OPENSSL=ON

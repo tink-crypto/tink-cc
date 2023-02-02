76af53c992dc82ffa9309f06445f8467f4f3b2cd
Moreno Ambrosin <ambrosin@google.com>
2023-02-02 08:14:18 -0800

    Add test script to build and test on Windows with Bazel
    PiperOrigin-RevId: 506624879
    Change-Id: I022abb1ab49ebb32f55ad4d8fb8dec2993b37dd3

---
+++ kokoro/gcp_windows/bazel/run_tests.bat
@@ -0,0 +1,35 @@
+:: Copyright 2023 Google LLC
+::
+:: Licensed under the Apache License, Version 2.0 (the "License");
+:: you may not use this file except in compliance with the License.
+:: You may obtain a copy of the License at
+::
+::      http://www.apache.org/licenses/LICENSE-2.0
+::
+:: Unless required by applicable law or agreed to in writing, software
+:: distributed under the License is distributed on an "AS IS" BASIS,
+:: WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+:: See the License for the specific language governing permissions and
+:: limitations under the License.
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+IF EXIST %KOKORO_ARTIFACTS_DIR%\git\tink_cc (
+  SET WORKSPACE_DIR=%KOKORO_ARTIFACTS_DIR%\git\tink_cc
+) ELSE IF EXIST %KOKORO_ARTIFACTS_DIR%\github\tink_cc (
+  SET WORKSPACE_DIR=%KOKORO_ARTIFACTS_DIR%\github\tink_cc
+)
+
+CD !WORKSPACE_DIR!
+if %errorlevel% neq 0 EXIT /B 1
+
+ECHO Build started at %TIME%
+bazel build ...
+IF %errorlevel% neq 0 EXIT /B 1
+ECHO Build completed at %TIME%
+
+ECHO Test started at %TIME%
+bazel test --strategy=TestRunner=standalone --test_output=errors -- ...
+IF %errorlevel% neq 0 EXIT /B 1
+ECHO Test completed at %TIME%
+
+EXIT /B 0

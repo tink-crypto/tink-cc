701cf5665dcfb407e1e4b75c94074422c9b6055a
Moreno Ambrosin <ambrosin@google.com>
2023-02-02 02:38:37 -0800

    Specify the correct version of the Visual Studio and added Copyright header
    PiperOrigin-RevId: 506563738
    Change-Id: I70bf42deb5d755f92b5f727ebec580bbdd0698fc

--- kokoro/gcp_windows/cmake/run_tests.bat
+++ kokoro/gcp_windows/cmake/run_tests.bat
@@ -1,3 +1,16 @@
+:: Copyright 2023 Google LLC
+::
+:: Licensed under the Apache License, Version 2.0 (the "License");
+:: you may not use this file except in compliance with the License.
+:: You may obtain a copy of the License at
+::
+::      http://www.apache.org/licenses/LICENSE-2.0
+::
+:: Unless required by applicable law or agreed to in writing, software
+:: distributed under the License is distributed on an "AS IS" BASIS,
+:: WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+:: See the License for the specific language governing permissions and
+:: limitations under the License.
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 SET CMAKE_BIN="C:\Program Files\CMake\bin\cmake.exe"
@@ -16,7 +29,7 @@ if %errorlevel% neq 0 EXIT /B 1
 MKDIR "build"
 CD "build"
 
-%CMAKE_BIN% -G"Visual Studio 15 2017 Win64" -S .. -B . ^
+%CMAKE_BIN% -G"Visual Studio 16 2019" -A x64 -S .. -B . ^
   -DCMAKE_CXX_STANDARD_REQUIRED=ON ^
   -DCMAKE_CXX_STANDARD=14 ^
   -DTINK_BUILD_TESTS=ON

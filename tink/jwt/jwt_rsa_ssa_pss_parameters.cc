89c3feaadde4cddd72c82787178379eb393f1c56
Ioana Nedelcu <ioannanedelcu@google.com>
2024-06-17 04:23:46 -0700

    Add JWT RSASSA-PSS parameters class and tests.
    PiperOrigin-RevId: 643957424
    Change-Id: Ie017f94820197bab1827ae2ae31d04ef663ccd4e

---
+++ tink/jwt/jwt_rsa_ssa_pss_parameters.cc
@@ -0,0 +1,154 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "tink/jwt/jwt_rsa_ssa_pss_parameters.h"
+
+#include <string>
+
+#include "absl/container/flat_hash_set.h"
+#include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/string_view.h"
+#ifdef OPENSSL_IS_BORINGSSL
+#include "openssl/base.h"
+#else
+#include "openssl/bn.h"
+#endif
+#include "tink/big_integer.h"
+#include "tink/internal/bn_util.h"
+#include "tink/internal/rsa_util.h"
+#include "tink/internal/ssl_unique_ptr.h"
+#include "tink/parameters.h"
+#include "tink/util/status.h"
+#include "tink/util/statusor.h"
+
+namespace crypto {
+namespace tink {
+namespace {
+
+constexpr int kF4 = 65537;
+
+}  // namespace
+
+JwtRsaSsaPssParameters::Builder&
+JwtRsaSsaPssParameters::Builder::SetAlgorithm(Algorithm algorithm) {
+  algorithm_ = algorithm;
+  return *this;
+}
+
+JwtRsaSsaPssParameters::Builder&
+JwtRsaSsaPssParameters::Builder::SetKidStrategy(KidStrategy kid_strategy) {
+  kid_strategy_ = kid_strategy;
+  return *this;
+}
+
+JwtRsaSsaPssParameters::Builder&
+JwtRsaSsaPssParameters::Builder::SetModulusSizeInBits(
+    int modulus_size_in_bits) {
+  modulus_size_in_bits_ = modulus_size_in_bits;
+  return *this;
+}
+
+JwtRsaSsaPssParameters::Builder&
+JwtRsaSsaPssParameters::Builder::SetPublicExponent(
+    const BigInteger& public_exponent) {
+  public_exponent_ = public_exponent;
+  return *this;
+}
+
+util::StatusOr<JwtRsaSsaPssParameters>
+JwtRsaSsaPssParameters::Builder::Build() {
+  if (!algorithm_.has_value()) {
+    return util::Status(absl::StatusCode::kInvalidArgument,
+                        "Algorithm is not set.");
+  }
+
+  if (!kid_strategy_.has_value()) {
+    return util::Status(absl::StatusCode::kInvalidArgument,
+                        "Kid strategy is not set.");
+  }
+
+  if (!modulus_size_in_bits_.has_value()) {
+    return util::Status(absl::StatusCode::kInvalidArgument,
+                        "Key size is not set.");
+  }
+
+  // Validate modulus size.
+  if (*modulus_size_in_bits_ < 2048) {
+    return util::Status(
+        absl::StatusCode::kInvalidArgument,
+        absl::StrCat("Invalid key size: must be at least 2048 bits, got ",
+                     *modulus_size_in_bits_, " bits."));
+  }
+
+  // Validate the public exponent: public exponent needs to be odd, greater than
+  // 65536 and (for consistency with BoringSSL), smaller that 32 bits.
+  util::Status exponent_status =
+      internal::ValidateRsaPublicExponent(public_exponent_.GetValue());
+  if (!exponent_status.ok()) {
+    return exponent_status;
+  }
+
+  // Validate algorithm.
+  static const auto kSupportedAlgorithms = new absl::flat_hash_set<Algorithm>(
+      {Algorithm::kPs256, Algorithm::kPs384, Algorithm::kPs512});
+  if (!kSupportedAlgorithms->contains(*algorithm_)) {
+    return util::Status(
+        absl::StatusCode::kInvalidArgument,
+        "Cannot create JWT RSASSA-PSS parameters with unknown algorithm.");
+  }
+
+  // Validate kid strategy.
+  static const auto kSupportedKidStrategies =
+      new absl::flat_hash_set<KidStrategy>({KidStrategy::kBase64EncodedKeyId,
+                                            KidStrategy::kIgnored,
+                                            KidStrategy::kCustom});
+  if (!kSupportedKidStrategies->contains(*kid_strategy_)) {
+    return util::Status(
+        absl::StatusCode::kInvalidArgument,
+        "Cannot create JWT RSASSA-PSS parameters with unknown kid strategy.");
+  }
+
+  return JwtRsaSsaPssParameters(*algorithm_, *kid_strategy_,
+                                  *modulus_size_in_bits_, public_exponent_);
+}
+
+bool JwtRsaSsaPssParameters::operator==(const Parameters& other) const {
+  const JwtRsaSsaPssParameters* that =
+      dynamic_cast<const JwtRsaSsaPssParameters*>(&other);
+  if (that == nullptr) {
+    return false;
+  }
+
+  return algorithm_ == that->algorithm_ &&
+         kid_strategy_ == that->kid_strategy_ &&
+         modulus_size_in_bits_ == that->modulus_size_in_bits_ &&
+         public_exponent_ == that->public_exponent_;
+}
+
+// Returns the big endian encoded F4 value, which is the default value of the
+// public exponent.
+BigInteger JwtRsaSsaPssParameters::Builder::CreateDefaultPublicExponent() {
+  internal::SslUniquePtr<BIGNUM> e(BN_new());
+  BN_set_word(e.get(), kF4);
+
+  std::string F4_string =
+      internal::BignumToString(e.get(), BN_num_bytes(e.get())).value();
+  return BigInteger(F4_string);
+}
+
+}  // namespace tink
+}  // namespace crypto
---
+++ tink/jwt/jwt_rsa_ssa_pss_parameters_test.cc
@@ -0,0 +1,452 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "tink/jwt/jwt_rsa_ssa_pss_parameters.h"
+
+#include <cstdint>
+#include <string>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "absl/status/status.h"
+#ifdef OPENSSL_IS_BORINGSSL
+#include "openssl/base.h"
+#else
+#include "openssl/bn.h"
+#endif
+#include "tink/big_integer.h"
+#include "tink/internal/bn_util.h"
+#include "tink/internal/ssl_unique_ptr.h"
+#include "tink/util/statusor.h"
+#include "tink/util/test_matchers.h"
+
+namespace crypto {
+namespace tink {
+namespace {
+
+using ::crypto::tink::test::IsOk;
+using ::crypto::tink::test::StatusIs;
+using ::testing::Eq;
+using ::testing::HasSubstr;
+using ::testing::IsTrue;
+using ::testing::TestWithParam;
+using ::testing::Values;
+
+struct TestCase {
+  JwtRsaSsaPssParameters::Algorithm algorithm;
+  JwtRsaSsaPssParameters::KidStrategy kid_strategy;
+  int modulus_size_in_bits;
+  bool has_id_requirement;
+  bool allow_kid_absent;
+};
+
+using JwtRsaSsaPssParametersTest = TestWithParam<TestCase>;
+
+std::string PublicExponentToString(int64_t public_exponent) {
+  internal::SslUniquePtr<BIGNUM> e(BN_new());
+  BN_set_word(e.get(), public_exponent);
+
+  return internal::BignumToString(e.get(), BN_num_bytes(e.get())).value();
+}
+
+const BigInteger& kF4 = *(new BigInteger(PublicExponentToString(65537)));
+
+INSTANTIATE_TEST_SUITE_P(
+    JwtRsaSsaPssParametersTestSuite, JwtRsaSsaPssParametersTest,
+    Values(TestCase{JwtRsaSsaPssParameters::Algorithm::kPs256,
+                    JwtRsaSsaPssParameters::KidStrategy::kBase64EncodedKeyId,
+                    /*modulus_size_in_bits=*/2048,
+                    /*has_id_requirement=*/true,
+                    /*allowed_kid_absent=*/false},
+           TestCase{JwtRsaSsaPssParameters::Algorithm::kPs384,
+                    JwtRsaSsaPssParameters::KidStrategy::kCustom,
+                    /*modulus_size_in_bits=*/3072,
+                    /*has_id_requirement=*/false, /*allowed_kid_absent=*/true},
+           TestCase{JwtRsaSsaPssParameters::Algorithm::kPs512,
+                    JwtRsaSsaPssParameters::KidStrategy::kIgnored,
+                    /*modulus_size_in_bits=*/2048,
+                    /*has_id_requirement=*/false,
+                    /*allowed_kid_absent=*/true}));
+
+TEST_P(JwtRsaSsaPssParametersTest, BuildWorks) {
+  TestCase test_case = GetParam();
+
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(test_case.modulus_size_in_bits)
+          .SetPublicExponent(kF4)
+          .SetAlgorithm(test_case.algorithm)
+          .SetKidStrategy(test_case.kid_strategy)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  EXPECT_THAT(parameters->GetModulusSizeInBits(),
+              Eq(test_case.modulus_size_in_bits));
+  EXPECT_THAT(parameters->GetPublicExponent(), Eq(kF4));
+  EXPECT_THAT(parameters->GetAlgorithm(), Eq(test_case.algorithm));
+  EXPECT_THAT(parameters->GetKidStrategy(), Eq(test_case.kid_strategy));
+  EXPECT_THAT(parameters->HasIdRequirement(), Eq(test_case.has_id_requirement));
+  EXPECT_THAT(parameters->AllowKidAbsent(), Eq(test_case.allow_kid_absent));
+}
+
+TEST(JwtRsaSsaPssParametersTest, BuildWithInvalidKidStrategyFails) {
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(2048)
+          .SetPublicExponent(kF4)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs256)
+          .SetKidStrategy(
+              JwtRsaSsaPssParameters::KidStrategy::
+                  kDoNotUseInsteadUseDefaultWhenWritingSwitchStatements)
+          .Build();
+  EXPECT_THAT(parameters.status(), StatusIs(absl::StatusCode::kInvalidArgument,
+                                            HasSubstr("unknown kid strategy")));
+}
+
+TEST(JwtRsaSsaPssParametersTest, BuildWithoutKidStrategyFails) {
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(2048)
+          .SetPublicExponent(kF4)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs256)
+          .Build();
+  EXPECT_THAT(parameters.status(),
+              StatusIs(absl::StatusCode::kInvalidArgument,
+                       HasSubstr("Kid strategy is not set")));
+}
+
+TEST(JwtRsaSsaPssParametersTest, BuildWithInvalidAlgorithmFails) {
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(2048)
+          .SetPublicExponent(kF4)
+          .SetAlgorithm(
+              JwtRsaSsaPssParameters::Algorithm::
+                  kDoNotUseInsteadUseDefaultWhenWritingSwitchStatements)
+          .SetKidStrategy(JwtRsaSsaPssParameters::KidStrategy::kIgnored)
+          .Build();
+  EXPECT_THAT(parameters.status(), StatusIs(absl::StatusCode::kInvalidArgument,
+                                            HasSubstr("unknown algorithm")));
+}
+
+TEST(JwtRsaSsaPssParametersTest, BuildWithoutAlgorithmFails) {
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(2048)
+          .SetPublicExponent(kF4)
+          .SetKidStrategy(JwtRsaSsaPssParameters::KidStrategy::kIgnored)
+          .Build();
+  EXPECT_THAT(parameters.status(), StatusIs(absl::StatusCode::kInvalidArgument,
+                                            HasSubstr("Algorithm is not set")));
+}
+
+TEST(JwtRsaSsaPssParametersTest, BuildWithLargeModulusSizeWorks) {
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(16789)
+          .SetPublicExponent(kF4)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs256)
+          .SetKidStrategy(JwtRsaSsaPssParameters::KidStrategy::kIgnored)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+  EXPECT_THAT(parameters->GetModulusSizeInBits(), Eq(16789));
+}
+
+TEST(JwtRsaSsaPssParametersTest, BuildWithTooSmallModulusSizeFails) {
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(2047)
+          .SetPublicExponent(kF4)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs256)
+          .SetKidStrategy(JwtRsaSsaPssParameters::KidStrategy::kIgnored)
+          .Build();
+  EXPECT_THAT(
+      parameters.status(),
+      StatusIs(absl::StatusCode::kInvalidArgument,
+               HasSubstr("Invalid key size: must be at least 2048 bits")));
+}
+
+TEST(JwtRsaSsaPssParametersTest, BuildWithoutModulusSizeFails) {
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetPublicExponent(kF4)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs256)
+          .SetKidStrategy(JwtRsaSsaPssParameters::KidStrategy::kIgnored)
+          .Build();
+  EXPECT_THAT(parameters.status(), StatusIs(absl::StatusCode::kInvalidArgument,
+                                            HasSubstr("Key size is not set")));
+}
+
+TEST(JwtRsaSsaPssParametersTest, BuildWithValidNonF4PublicExponentWorks) {
+  BigInteger nonF4_public_exponent =
+      BigInteger(PublicExponentToString(1234567));
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(2048)
+          .SetPublicExponent(nonF4_public_exponent)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs256)
+          .SetKidStrategy(JwtRsaSsaPssParameters::KidStrategy::kIgnored)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+  EXPECT_THAT(parameters->GetPublicExponent(), Eq(nonF4_public_exponent));
+}
+
+TEST(JwtRsaSsaPssParametersTest, BuildWithoutPublicExponentDefaultsToF4) {
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(2048)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs256)
+          .SetKidStrategy(JwtRsaSsaPssParameters::KidStrategy::kIgnored)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+  EXPECT_THAT(parameters->GetPublicExponent(), Eq(kF4));
+}
+
+TEST(JwtRsaSsaPssParametersTest, BuildWithLargePublicExponentWorks) {
+  BigInteger large_public_exponent =
+      BigInteger(PublicExponentToString(100000001L));
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(2048)
+          .SetPublicExponent(large_public_exponent)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs256)
+          .SetKidStrategy(JwtRsaSsaPssParameters::KidStrategy::kIgnored)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+  EXPECT_THAT(parameters->GetPublicExponent(), Eq(large_public_exponent));
+}
+
+TEST(JwtRsaSsaPssParametersTest, BuildWithEvenPublicExponentFails) {
+  BigInteger even_public_exponent = BigInteger(PublicExponentToString(123456));
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(2048)
+          .SetPublicExponent(even_public_exponent)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs256)
+          .SetKidStrategy(JwtRsaSsaPssParameters::KidStrategy::kIgnored)
+          .Build();
+  EXPECT_THAT(parameters.status(),
+              StatusIs(absl::StatusCode::kInvalidArgument,
+                       HasSubstr("Public exponent must be odd")));
+}
+
+TEST(JwtRsaSsaPssParametersTest, BuildWithTooSmallPublicExponentFails) {
+  BigInteger small_public_exponent = BigInteger(PublicExponentToString(3));
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(2048)
+          .SetPublicExponent(small_public_exponent)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs256)
+          .SetKidStrategy(JwtRsaSsaPssParameters::KidStrategy::kIgnored)
+          .Build();
+  EXPECT_THAT(
+      parameters.status(),
+      StatusIs(absl::StatusCode::kInvalidArgument,
+               HasSubstr("Public exponent must be greater than 65536")));
+}
+
+TEST(JwtRsaSsaPssParametersTest, BuildWithTooLargePublicExponentFails) {
+  // Public exponent must have less than 32 bits.
+  BigInteger too_large_public_exponent =
+      BigInteger(PublicExponentToString(4294967297L));
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(2048)
+          .SetPublicExponent(too_large_public_exponent)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs256)
+          .SetKidStrategy(JwtRsaSsaPssParameters::KidStrategy::kIgnored)
+          .Build();
+  EXPECT_THAT(
+      parameters.status(),
+      StatusIs(absl::StatusCode::kInvalidArgument,
+               HasSubstr("Exponent size must be smaller than 32 bits")));
+}
+
+TEST(JwtRsaSsaPssParametersTest, CopyConstructor) {
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(2048)
+          .SetPublicExponent(kF4)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs512)
+          .SetKidStrategy(
+              JwtRsaSsaPssParameters::KidStrategy::kBase64EncodedKeyId)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  JwtRsaSsaPssParameters copy(*parameters);
+
+  EXPECT_THAT(copy.GetKidStrategy(),
+              Eq(JwtRsaSsaPssParameters::KidStrategy::kBase64EncodedKeyId));
+  EXPECT_THAT(copy.HasIdRequirement(), IsTrue());
+  EXPECT_THAT(parameters->GetModulusSizeInBits(), Eq(2048));
+  EXPECT_THAT(parameters->GetPublicExponent(), Eq(kF4));
+  EXPECT_THAT(parameters->GetAlgorithm(),
+              Eq(JwtRsaSsaPssParameters::Algorithm::kPs512));
+}
+
+TEST(JwtRsaSsaPssParametersTest, CopyAssignment) {
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(2048)
+          .SetPublicExponent(kF4)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs512)
+          .SetKidStrategy(
+              JwtRsaSsaPssParameters::KidStrategy::kBase64EncodedKeyId)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  JwtRsaSsaPssParameters copy = *parameters;
+
+  EXPECT_THAT(copy.GetKidStrategy(),
+              Eq(JwtRsaSsaPssParameters::KidStrategy::kBase64EncodedKeyId));
+  EXPECT_THAT(copy.HasIdRequirement(), IsTrue());
+  EXPECT_THAT(parameters->GetModulusSizeInBits(), Eq(2048));
+  EXPECT_THAT(parameters->GetPublicExponent(), Eq(kF4));
+  EXPECT_THAT(parameters->GetAlgorithm(),
+              Eq(JwtRsaSsaPssParameters::Algorithm::kPs512));
+}
+
+TEST_P(JwtRsaSsaPssParametersTest, ParametersEquals) {
+  TestCase test_case = GetParam();
+
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(test_case.modulus_size_in_bits)
+          .SetPublicExponent(kF4)
+          .SetAlgorithm(test_case.algorithm)
+          .SetKidStrategy(test_case.kid_strategy)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  util::StatusOr<JwtRsaSsaPssParameters> other_parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(test_case.modulus_size_in_bits)
+          .SetPublicExponent(kF4)
+          .SetAlgorithm(test_case.algorithm)
+          .SetKidStrategy(test_case.kid_strategy)
+          .Build();
+  ASSERT_THAT(other_parameters, IsOk());
+
+  EXPECT_TRUE(*parameters == *other_parameters);
+  EXPECT_TRUE(*other_parameters == *parameters);
+  EXPECT_FALSE(*parameters != *other_parameters);
+  EXPECT_FALSE(*other_parameters != *parameters);
+}
+
+TEST(JwtRsaSsaPssParametersTest, KidStrategyNotEqual) {
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(2048)
+          .SetPublicExponent(kF4)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs512)
+          .SetKidStrategy(
+              JwtRsaSsaPssParameters::KidStrategy::kBase64EncodedKeyId)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  util::StatusOr<JwtRsaSsaPssParameters> other_parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(2048)
+          .SetPublicExponent(kF4)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs512)
+          .SetKidStrategy(JwtRsaSsaPssParameters::KidStrategy::kCustom)
+          .Build();
+  ASSERT_THAT(other_parameters, IsOk());
+
+  EXPECT_TRUE(*parameters != *other_parameters);
+  EXPECT_FALSE(*parameters == *other_parameters);
+}
+
+TEST(JwtRsaSsaPssParametersTest, AlgorithmNotEqual) {
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(2048)
+          .SetPublicExponent(kF4)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs256)
+          .SetKidStrategy(
+              JwtRsaSsaPssParameters::KidStrategy::kBase64EncodedKeyId)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  util::StatusOr<JwtRsaSsaPssParameters> other_parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(2048)
+          .SetPublicExponent(kF4)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs384)
+          .SetKidStrategy(
+              JwtRsaSsaPssParameters::KidStrategy::kBase64EncodedKeyId)
+          .Build();
+  ASSERT_THAT(other_parameters, IsOk());
+
+  EXPECT_TRUE(*parameters != *other_parameters);
+  EXPECT_FALSE(*parameters == *other_parameters);
+}
+
+TEST(JwtRsaSsaPssParametersTest, ModulusSizeNotEqual) {
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(2048)
+          .SetPublicExponent(kF4)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs256)
+          .SetKidStrategy(
+              JwtRsaSsaPssParameters::KidStrategy::kBase64EncodedKeyId)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  util::StatusOr<JwtRsaSsaPssParameters> other_parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetModulusSizeInBits(3072)
+          .SetPublicExponent(kF4)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs256)
+          .SetKidStrategy(
+              JwtRsaSsaPssParameters::KidStrategy::kBase64EncodedKeyId)
+          .Build();
+  ASSERT_THAT(other_parameters, IsOk());
+
+  EXPECT_TRUE(*parameters != *other_parameters);
+  EXPECT_FALSE(*parameters == *other_parameters);
+}
+
+TEST(JwtRsaSsaPssParametersTest, PublicExponentNotEqual) {
+  util::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetKidStrategy(
+              JwtRsaSsaPssParameters::KidStrategy::kBase64EncodedKeyId)
+          .SetModulusSizeInBits(2048)
+          .SetPublicExponent(kF4)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs256)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  BigInteger nonF4_public_exponent =
+      BigInteger(PublicExponentToString(1234567));
+  util::StatusOr<JwtRsaSsaPssParameters> other_parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetKidStrategy(
+              JwtRsaSsaPssParameters::KidStrategy::kBase64EncodedKeyId)
+          .SetModulusSizeInBits(2048)
+          .SetPublicExponent(nonF4_public_exponent)
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs256)
+          .Build();
+  ASSERT_THAT(other_parameters, IsOk());
+
+  EXPECT_TRUE(*parameters != *other_parameters);
+  EXPECT_FALSE(*parameters == *other_parameters);
+}
+
+}  // namespace
+}  // namespace tink
+}  // namespace crypto

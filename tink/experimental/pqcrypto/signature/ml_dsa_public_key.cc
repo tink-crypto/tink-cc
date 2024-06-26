271998b7a0148cc392caf5e764a733927d423fe7
Guillaume Endignoux <guillaumee@google.com>
2024-06-26 03:58:11 -0700

    Add public key type for ML-DSA.
    PiperOrigin-RevId: 646811759
    Change-Id: Ie5e083f75b7f1882dcd928d5e86014691eb5cd90

---
+++ tink/experimental/pqcrypto/signature/ml_dsa_public_key.cc
@@ -0,0 +1,107 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "tink/experimental/pqcrypto/signature/ml_dsa_public_key.h"
+
+#include <string>
+
+#include "absl/status/status.h"
+#include "absl/strings/escaping.h"
+#include "absl/strings/str_cat.h"
+#include "absl/types/optional.h"
+#define OPENSSL_UNSTABLE_EXPERIMENTAL_DILITHIUM
+#include "openssl/experimental/dilithium.h"
+#include "tink/experimental/pqcrypto/signature/ml_dsa_parameters.h"
+#include "tink/key.h"
+#include "tink/subtle/subtle_util.h"
+#include "tink/util/status.h"
+#include "tink/util/statusor.h"
+
+namespace crypto {
+namespace tink {
+namespace {
+
+util::StatusOr<std::string> ComputeOutputPrefix(
+    const MlDsaParameters& parameters, absl::optional<int> id_requirement) {
+  switch (parameters.GetVariant()) {
+    case MlDsaParameters::Variant::kNoPrefix:
+      return std::string("");  // Empty prefix.
+    case MlDsaParameters::Variant::kTink:
+      if (!id_requirement.has_value()) {
+        return util::Status(absl::StatusCode::kInvalidArgument,
+                            "ID requirement must have value with kTink");
+      }
+      return absl::StrCat(absl::HexStringToBytes("01"),
+                          subtle::BigEndian32(*id_requirement));
+    default:
+      return util::Status(
+          absl::StatusCode::kInvalidArgument,
+          absl::StrCat("Invalid variant: ", parameters.GetVariant()));
+  }
+}
+
+}  // namespace
+
+util::StatusOr<MlDsaPublicKey> MlDsaPublicKey::Create(
+    const MlDsaParameters& parameters, absl::string_view public_key_bytes,
+    absl::optional<int> id_requirement, PartialKeyAccessToken token) {
+  if (parameters.HasIdRequirement() && !id_requirement.has_value()) {
+    return util::Status(
+        absl::StatusCode::kInvalidArgument,
+        "Cannot create key without ID requirement with parameters with ID "
+        "requirement");
+  }
+  if (!parameters.HasIdRequirement() && id_requirement.has_value()) {
+    return util::Status(
+        absl::StatusCode::kInvalidArgument,
+        "Cannot create key with ID requirement with parameters without ID "
+        "requirement");
+  }
+
+  if (parameters.GetInstance() != MlDsaParameters::Instance::kMlDsa65) {
+    return util::Status(absl::StatusCode::kInvalidArgument,
+                        "Invalid ML-DSA instance. Only ML-DSA-65 is "
+                        "currently supported.");
+  }
+
+  if (public_key_bytes.size() != DILITHIUM_PUBLIC_KEY_BYTES) {
+    return util::Status(absl::StatusCode::kInvalidArgument,
+                        absl::StrCat("Invalid ML-DSA public key size. Only ",
+                                     DILITHIUM_PUBLIC_KEY_BYTES,
+                                     "-byte keys are currently supported."));
+  }
+
+  util::StatusOr<std::string> output_prefix =
+      ComputeOutputPrefix(parameters, id_requirement);
+  if (!output_prefix.ok()) {
+    return output_prefix.status();
+  }
+  return MlDsaPublicKey(parameters, public_key_bytes, id_requirement,
+                        *output_prefix);
+}
+
+bool MlDsaPublicKey::operator==(const Key& other) const {
+  const MlDsaPublicKey* that = dynamic_cast<const MlDsaPublicKey*>(&other);
+  if (that == nullptr) {
+    return false;
+  }
+  return GetParameters() == that->GetParameters() &&
+         public_key_bytes_ == that->public_key_bytes_ &&
+         id_requirement_ == that->id_requirement_;
+}
+
+}  // namespace tink
+}  // namespace crypto
---
+++ tink/experimental/pqcrypto/signature/ml_dsa_public_key_test.cc
@@ -0,0 +1,231 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "tink/experimental/pqcrypto/signature/ml_dsa_public_key.h"
+
+#include <cstdint>
+#include <string>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
+#include "absl/types/optional.h"
+#define OPENSSL_UNSTABLE_EXPERIMENTAL_DILITHIUM
+#include "openssl/experimental/dilithium.h"
+#include "tink/experimental/pqcrypto/signature/ml_dsa_parameters.h"
+#include "tink/partial_key_access.h"
+#include "tink/util/secret_data.h"
+#include "tink/util/statusor.h"
+#include "tink/util/test_matchers.h"
+
+namespace crypto {
+namespace tink {
+namespace {
+
+using ::crypto::tink::test::IsOk;
+using ::crypto::tink::test::StatusIs;
+using ::testing::Eq;
+using ::testing::HasSubstr;
+using ::testing::TestWithParam;
+using ::testing::Values;
+
+struct TestCase {
+  MlDsaParameters::Variant variant;
+  absl::optional<int> id_requirement;
+  std::string output_prefix;
+};
+
+using MlDsaPublicKeyTest = TestWithParam<TestCase>;
+
+INSTANTIATE_TEST_SUITE_P(
+    MlDsaPublicKeyTestSuite, MlDsaPublicKeyTest,
+    Values(TestCase{MlDsaParameters::Variant::kTink, 0x02030400,
+                    std::string("\x01\x02\x03\x04\x00", 5)},
+           TestCase{MlDsaParameters::Variant::kTink, 0x03050709,
+                    std::string("\x01\x03\x05\x07\x09", 5)},
+           TestCase{MlDsaParameters::Variant::kNoPrefix, absl::nullopt, ""}));
+
+using MlDsaPublicKeyTest = TestWithParam<TestCase>;
+
+std::string GeneratePublicKey() {
+  std::string public_key_bytes;
+  public_key_bytes.resize(DILITHIUM_PUBLIC_KEY_BYTES);
+  auto bssl_private_key = util::MakeSecretUniquePtr<DILITHIUM_private_key>();
+
+  DILITHIUM_generate_key(reinterpret_cast<uint8_t *>(&public_key_bytes[0]),
+                         bssl_private_key.get());
+
+  return public_key_bytes;
+}
+
+TEST_P(MlDsaPublicKeyTest, CreatePublicKeyWorks) {
+  TestCase test_case = GetParam();
+
+  util::StatusOr<MlDsaParameters> parameters = MlDsaParameters::Create(
+      MlDsaParameters::Instance::kMlDsa65, test_case.variant);
+  ASSERT_THAT(parameters, IsOk());
+
+  std::string public_key_bytes = GeneratePublicKey();
+  util::StatusOr<MlDsaPublicKey> public_key =
+      MlDsaPublicKey::Create(*parameters, public_key_bytes,
+                             test_case.id_requirement, GetPartialKeyAccess());
+  ASSERT_THAT(public_key, IsOk());
+
+  EXPECT_THAT(public_key->GetParameters(), Eq(*parameters));
+  EXPECT_THAT(public_key->GetIdRequirement(), Eq(test_case.id_requirement));
+  EXPECT_THAT(public_key->GetOutputPrefix(), Eq(test_case.output_prefix));
+  EXPECT_THAT(public_key->GetPublicKeyBytes(GetPartialKeyAccess()),
+              Eq(public_key_bytes));
+}
+
+TEST_P(MlDsaPublicKeyTest, CreateWithInvalidPublicKeyLengthFails) {
+  TestCase test_case = GetParam();
+
+  util::StatusOr<MlDsaParameters> parameters = MlDsaParameters::Create(
+      MlDsaParameters::Instance::kMlDsa65, test_case.variant);
+  ASSERT_THAT(parameters, IsOk());
+
+  std::string public_key_bytes = GeneratePublicKey();
+  EXPECT_THAT(
+      MlDsaPublicKey::Create(
+          *parameters,
+          public_key_bytes.substr(0, DILITHIUM_PUBLIC_KEY_BYTES - 1),
+          test_case.id_requirement, GetPartialKeyAccess())
+          .status(),
+      StatusIs(absl::StatusCode::kInvalidArgument,
+               HasSubstr(absl::StrCat("Invalid ML-DSA public key size. Only ",
+                                      DILITHIUM_PUBLIC_KEY_BYTES,
+                                      "-byte keys are currently supported."))));
+
+  public_key_bytes.push_back(0);
+  EXPECT_THAT(
+      MlDsaPublicKey::Create(*parameters, public_key_bytes,
+                             test_case.id_requirement, GetPartialKeyAccess())
+          .status(),
+      StatusIs(absl::StatusCode::kInvalidArgument,
+               HasSubstr(absl::StrCat("Invalid ML-DSA public key size. Only ",
+                                      DILITHIUM_PUBLIC_KEY_BYTES,
+                                      "-byte keys are currently supported."))));
+}
+
+TEST(MlDsaPublicKeyTest, CreateKeyWithNoIdRequirementWithTinkParamsFails) {
+  util::StatusOr<MlDsaParameters> parameters = MlDsaParameters::Create(
+      MlDsaParameters::Instance::kMlDsa65, MlDsaParameters::Variant::kTink);
+  ASSERT_THAT(parameters, IsOk());
+
+  std::string public_key_bytes = GeneratePublicKey();
+  EXPECT_THAT(MlDsaPublicKey::Create(*parameters, public_key_bytes,
+                                     /*id_requirement=*/absl::nullopt,
+                                     GetPartialKeyAccess())
+                  .status(),
+              StatusIs(absl::StatusCode::kInvalidArgument,
+                       HasSubstr("key without ID requirement with parameters "
+                                 "with ID requirement")));
+}
+
+TEST(MlDsaPublicKeyTest, CreateKeyWithIdRequirementWithNoPrefixParamsFails) {
+  util::StatusOr<MlDsaParameters> parameters = MlDsaParameters::Create(
+      MlDsaParameters::Instance::kMlDsa65, MlDsaParameters::Variant::kNoPrefix);
+  ASSERT_THAT(parameters, IsOk());
+
+  std::string public_key_bytes = GeneratePublicKey();
+  EXPECT_THAT(
+      MlDsaPublicKey::Create(*parameters, public_key_bytes,
+                             /*id_requirement=*/123, GetPartialKeyAccess())
+          .status(),
+      StatusIs(absl::StatusCode::kInvalidArgument,
+               HasSubstr("key with ID requirement with parameters without ID "
+                         "requirement")));
+}
+
+TEST_P(MlDsaPublicKeyTest, PublicKeyEquals) {
+  TestCase test_case = GetParam();
+
+  util::StatusOr<MlDsaParameters> parameters = MlDsaParameters::Create(
+      MlDsaParameters::Instance::kMlDsa65, test_case.variant);
+  ASSERT_THAT(parameters, IsOk());
+
+  std::string public_key_bytes = GeneratePublicKey();
+
+  util::StatusOr<MlDsaPublicKey> public_key =
+      MlDsaPublicKey::Create(*parameters, public_key_bytes,
+                             test_case.id_requirement, GetPartialKeyAccess());
+  ASSERT_THAT(public_key, IsOk());
+
+  util::StatusOr<MlDsaPublicKey> other_public_key =
+      MlDsaPublicKey::Create(*parameters, public_key_bytes,
+                             test_case.id_requirement, GetPartialKeyAccess());
+  ASSERT_THAT(other_public_key, IsOk());
+
+  EXPECT_TRUE(*public_key == *other_public_key);
+  EXPECT_TRUE(*other_public_key == *public_key);
+  EXPECT_FALSE(*public_key != *other_public_key);
+  EXPECT_FALSE(*other_public_key != *public_key);
+}
+
+TEST_P(MlDsaPublicKeyTest, DifferentPublicKeyBytesNotEqual) {
+  TestCase test_case = GetParam();
+
+  util::StatusOr<MlDsaParameters> parameters = MlDsaParameters::Create(
+      MlDsaParameters::Instance::kMlDsa65, test_case.variant);
+  ASSERT_THAT(parameters, IsOk());
+
+  std::string public_key_bytes1 = GeneratePublicKey();
+  std::string public_key_bytes2 = GeneratePublicKey();
+
+  util::StatusOr<MlDsaPublicKey> public_key1 =
+      MlDsaPublicKey::Create(*parameters, public_key_bytes1,
+                             test_case.id_requirement, GetPartialKeyAccess());
+  ASSERT_THAT(public_key1, IsOk());
+
+  util::StatusOr<MlDsaPublicKey> public_key2 =
+      MlDsaPublicKey::Create(*parameters, public_key_bytes2,
+                             test_case.id_requirement, GetPartialKeyAccess());
+  ASSERT_THAT(public_key2, IsOk());
+
+  EXPECT_TRUE(*public_key1 != *public_key2);
+  EXPECT_TRUE(*public_key2 != *public_key1);
+  EXPECT_FALSE(*public_key1 == *public_key2);
+  EXPECT_FALSE(*public_key2 == *public_key1);
+}
+
+TEST(MlDsaPublicKeyTest, DifferentIdRequirementNotEqual) {
+  util::StatusOr<MlDsaParameters> parameters = MlDsaParameters::Create(
+      MlDsaParameters::Instance::kMlDsa65, MlDsaParameters::Variant::kTink);
+  ASSERT_THAT(parameters, IsOk());
+
+  std::string public_key_bytes = GeneratePublicKey();
+
+  util::StatusOr<MlDsaPublicKey> public_key =
+      MlDsaPublicKey::Create(*parameters, public_key_bytes,
+                             /*id_requirement=*/123, GetPartialKeyAccess());
+  ASSERT_THAT(public_key, IsOk());
+
+  util::StatusOr<MlDsaPublicKey> other_public_key =
+      MlDsaPublicKey::Create(*parameters, public_key_bytes,
+                             /*id_requirement=*/456, GetPartialKeyAccess());
+  ASSERT_THAT(other_public_key, IsOk());
+
+  EXPECT_TRUE(*public_key != *other_public_key);
+  EXPECT_TRUE(*other_public_key != *public_key);
+  EXPECT_FALSE(*public_key == *other_public_key);
+  EXPECT_FALSE(*other_public_key == *public_key);
+}
+
+}  // namespace
+}  // namespace tink
+}  // namespace crypto

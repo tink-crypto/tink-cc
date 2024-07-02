3ce0bbfdc075fb2237646591ecdf526b72abbecc
William Conner <wconner@google.com>
2024-07-02 07:56:25 -0700

    Add interfaces for Streaming AEAD parameters and key types.
    PiperOrigin-RevId: 648718197
    Change-Id: I6310d2180ece7eccf03f1d32fc81c324a73f37c1

---
+++ tink/streamingaead/streaming_aead_key.h
@@ -0,0 +1,42 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef TINK_STREAMINGAEAD_STREAMING_AEAD_KEY_H_
+#define TINK_STREAMINGAEAD_STREAMING_AEAD_KEY_H_
+
+#include "absl/types/optional.h"
+#include "tink/key.h"
+#include "tink/streamingaead/streaming_aead_parameters.h"
+
+namespace crypto {
+namespace tink {
+
+// Represents a function to encrypt and decrypt data using Streaming AEAD.
+class StreamingAeadKey : public Key {
+ public:
+  const StreamingAeadParameters& GetParameters() const override = 0;
+
+  absl::optional<int> GetIdRequirement() const override {
+    return absl::nullopt;
+  }
+
+  bool operator==(const Key& other) const override = 0;
+};
+
+}  // namespace tink
+}  // namespace crypto
+
+#endif  // TINK_STREAMINGAEAD_STREAMING_AEAD_KEY_H_

a5d4053f82b9da883bc1c0cc36786bcd972ba8ce
William Conner <wconner@google.com>
2024-07-24 08:52:24 -0700

    Add AES-CMAC-PRF key type.
    PiperOrigin-RevId: 655584109
    Change-Id: Ie0936ee356112d0343613b6b7142fa96d15bbd12

---
+++ tink/prf/aes_cmac_prf_key.cc
@@ -0,0 +1,52 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "tink/prf/aes_cmac_prf_key.h"
+
+#include <utility>
+
+#include "tink/key.h"
+#include "tink/partial_key_access_token.h"
+#include "tink/prf/aes_cmac_prf_parameters.h"
+#include "tink/restricted_data.h"
+#include "tink/util/statusor.h"
+
+namespace crypto {
+namespace tink {
+
+util::StatusOr<AesCmacPrfKey> AesCmacPrfKey::Create(
+    RestrictedData key_bytes, PartialKeyAccessToken token) {
+  util::StatusOr<AesCmacPrfParameters> parameters =
+      AesCmacPrfParameters::Create(key_bytes.size());
+  if (!parameters.ok()) {
+    return parameters.status();
+  }
+  return AesCmacPrfKey(std::move(*parameters), std::move(key_bytes));
+}
+
+bool AesCmacPrfKey::operator==(const Key& other) const {
+  const AesCmacPrfKey* that = dynamic_cast<const AesCmacPrfKey*>(&other);
+  if (that == nullptr) {
+    return false;
+  }
+  if (GetParameters() != that->GetParameters()) {
+    return false;
+  }
+  return key_bytes_ == that->key_bytes_;
+}
+
+}  // namespace tink
+}  // namespace crypto
---
+++ tink/prf/aes_cmac_prf_key_test.cc
@@ -0,0 +1,110 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "tink/prf/aes_cmac_prf_key.h"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "absl/status/status.h"
+#include "absl/types/optional.h"
+#include "tink/partial_key_access.h"
+#include "tink/prf/aes_cmac_prf_parameters.h"
+#include "tink/restricted_data.h"
+#include "tink/util/statusor.h"
+#include "tink/util/test_matchers.h"
+
+namespace crypto {
+namespace tink {
+namespace {
+
+using ::crypto::tink::test::IsOk;
+using ::crypto::tink::test::StatusIs;
+using ::testing::Eq;
+using ::testing::TestWithParam;
+using ::testing::Values;
+
+using AesCmacPrfKeyTest = TestWithParam<int>;
+
+INSTANTIATE_TEST_SUITE_P(AesCmacPrfKeyTestSuite, AesCmacPrfKeyTest,
+                         Values(16, 32));
+
+TEST_P(AesCmacPrfKeyTest, CreateSucceeds) {
+  int key_size = GetParam();
+
+  RestrictedData secret = RestrictedData(key_size);
+  util::StatusOr<AesCmacPrfKey> key =
+      AesCmacPrfKey::Create(secret, GetPartialKeyAccess());
+  ASSERT_THAT(key, IsOk());
+
+  util::StatusOr<AesCmacPrfParameters> expected_parameters =
+      AesCmacPrfParameters::Create(key_size);
+  ASSERT_THAT(expected_parameters, IsOk());
+
+  EXPECT_THAT(key->GetKeyBytes(GetPartialKeyAccess()), Eq(secret));
+  EXPECT_THAT(key->GetParameters(), Eq(*expected_parameters));
+  EXPECT_THAT(key->GetIdRequirement(), Eq(absl::nullopt));
+}
+
+TEST(AesCmacPrfKeyTest, CreateKeyWithInvalidKeySizeFails) {
+  EXPECT_THAT(AesCmacPrfKey::Create(RestrictedData(/*num_random_bytes=*/17),
+                                    GetPartialKeyAccess())
+                  .status(),
+              StatusIs(absl::StatusCode::kInvalidArgument));
+  EXPECT_THAT(AesCmacPrfKey::Create(RestrictedData(/*num_random_bytes=*/33),
+                                    GetPartialKeyAccess())
+                  .status(),
+              StatusIs(absl::StatusCode::kInvalidArgument));
+}
+
+TEST_P(AesCmacPrfKeyTest, KeyEquals) {
+  int key_size = GetParam();
+
+  RestrictedData secret = RestrictedData(key_size);
+  util::StatusOr<AesCmacPrfKey> key =
+      AesCmacPrfKey::Create(secret, GetPartialKeyAccess());
+  ASSERT_THAT(key, IsOk());
+
+  util::StatusOr<AesCmacPrfKey> other_key =
+      AesCmacPrfKey::Create(secret, GetPartialKeyAccess());
+  ASSERT_THAT(other_key, IsOk());
+
+  EXPECT_TRUE(*key == *other_key);
+  EXPECT_TRUE(*other_key == *key);
+  EXPECT_FALSE(*key != *other_key);
+  EXPECT_FALSE(*other_key != *key);
+}
+
+TEST(AesCmacPrfKeyTest, DifferentSecretDataNotEqual) {
+  RestrictedData secret1 = RestrictedData(/*num_random_bytes=*/16);
+  RestrictedData secret2 = RestrictedData(/*num_random_bytes=*/16);
+
+  util::StatusOr<AesCmacPrfKey> key =
+      AesCmacPrfKey::Create(secret1, GetPartialKeyAccess());
+  ASSERT_THAT(key, IsOk());
+
+  util::StatusOr<AesCmacPrfKey> other_key =
+      AesCmacPrfKey::Create(secret2, GetPartialKeyAccess());
+  ASSERT_THAT(other_key, IsOk());
+
+  EXPECT_TRUE(*key != *other_key);
+  EXPECT_TRUE(*other_key != *key);
+  EXPECT_FALSE(*key == *other_key);
+  EXPECT_FALSE(*other_key == *key);
+}
+
+}  // namespace
+}  // namespace tink
+}  // namespace crypto

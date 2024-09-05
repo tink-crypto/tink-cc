773b143411af391daaefea595072f65fd109f161
Fernando Lobato <felobato@google.com>
2024-09-05 11:32:07 -0700

    X-AES-GCM Key Class
    PiperOrigin-RevId: 671447335
    Change-Id: Icac3b20fea2197768586814e98de01e34e1e53e8

---
+++ tink/aead/x_aes_gcm_key.h
@@ -0,0 +1,85 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef TINK_AEAD_X_AES_GCM_KEY_H_
+#define TINK_AEAD_X_AES_GCM_KEY_H_
+
+#include <cstdint>
+#include <string>
+#include <utility>
+
+#include "absl/strings/string_view.h"
+#include "absl/types/optional.h"
+#include "tink/aead/aead_key.h"
+#include "tink/aead/x_aes_gcm_parameters.h"
+#include "tink/key.h"
+#include "tink/partial_key_access_token.h"
+#include "tink/restricted_data.h"
+#include "tink/util/statusor.h"
+
+namespace crypto {
+namespace tink {
+
+// Represents an AEAD that uses X-AES-GCM.
+class XAesGcmKey : public AeadKey {
+ public:
+  // Copyable and movable.
+  XAesGcmKey(const XAesGcmKey& other) = default;
+  XAesGcmKey& operator=(const XAesGcmKey& other) = default;
+  XAesGcmKey(XAesGcmKey&& other) = default;
+  XAesGcmKey& operator=(XAesGcmKey&& other) = default;
+
+  // Creates a new X-AES-GCM key. If the parameters specify a variant that uses
+  // a prefix, then the id is used to compute this prefix.
+  static util::StatusOr<XAesGcmKey> Create(
+      XAesGcmParameters parameters, const RestrictedData& key_bytes,
+      absl::optional<int32_t> id_requirement, PartialKeyAccessToken token);
+
+  // Returns the underlying AES key.
+  const RestrictedData& GetKeyBytes(PartialKeyAccessToken token) const {
+    return key_bytes_;
+  }
+
+  absl::string_view GetOutputPrefix() const override { return output_prefix_; }
+
+  const XAesGcmParameters& GetParameters() const override {
+    return parameters_;
+  }
+
+  absl::optional<int32_t> GetIdRequirement() const override {
+    return id_requirement_;
+  }
+
+  bool operator==(const Key& other) const override;
+
+ private:
+  XAesGcmKey(XAesGcmParameters parameters, const RestrictedData& key_bytes,
+             absl::optional<int32_t> id_requirement, std::string output_prefix)
+      : parameters_(std::move(parameters)),
+        key_bytes_(key_bytes),
+        id_requirement_(id_requirement),
+        output_prefix_(std::move(output_prefix)) {}
+
+  XAesGcmParameters parameters_;
+  RestrictedData key_bytes_;
+  absl::optional<int32_t> id_requirement_;
+  std::string output_prefix_;
+};
+
+}  // namespace tink
+}  // namespace crypto
+
+#endif  // TINK_AEAD_X_AES_GCM_KEY_H_
---
+++ tink/aead/x_aes_gcm_key_test.cc
@@ -0,0 +1,243 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "tink/aead/x_aes_gcm_key.h"
+
+#include <string>
+#include <tuple>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "absl/status/status.h"
+#include "absl/types/optional.h"
+#include "tink/aead/x_aes_gcm_parameters.h"
+#include "tink/partial_key_access.h"
+#include "tink/restricted_data.h"
+#include "tink/util/statusor.h"
+#include "tink/util/test_matchers.h"
+
+namespace crypto {
+namespace tink {
+namespace {
+
+using ::crypto::tink::test::IsOk;
+using ::crypto::tink::test::StatusIs;
+using ::testing::Eq;
+using ::testing::TestWithParam;
+using ::testing::Values;
+
+constexpr int kKeySize = 32;
+constexpr int kSaltSizeBytes = 12;
+
+struct TestCase {
+  XAesGcmParameters::Variant variant;
+  int salt_size_bytes;
+  absl::optional<int> id_requirement;
+  std::string output_prefix;
+};
+
+using XAesGcmKeyTest = TestWithParam<TestCase>;
+
+INSTANTIATE_TEST_SUITE_P(
+    XAesGcmKeyTestSuite, XAesGcmKeyTest,
+    Values(TestCase{XAesGcmParameters::Variant::kTink, /*salt_size_bytes=*/8,
+                    0x02030400, std::string("\x01\x02\x03\x04\x00", 5)},
+           TestCase{XAesGcmParameters::Variant::kTink,
+                    /*salt_size_bytes=*/10, 0x01030005,
+                    std::string("\x01\x01\x03\x00\x05", 5)},
+           TestCase{XAesGcmParameters::Variant::kNoPrefix,
+                    /*salt_size_bytes=*/12, absl::nullopt, ""}));
+
+TEST_P(XAesGcmKeyTest, CreateSucceeds) {
+  TestCase test_case = GetParam();
+
+  util::StatusOr<XAesGcmParameters> params =
+      XAesGcmParameters::Create(test_case.variant, test_case.salt_size_bytes);
+  ASSERT_THAT(params, IsOk());
+
+  RestrictedData secret = RestrictedData(kKeySize);
+  util::StatusOr<XAesGcmKey> key = XAesGcmKey::Create(
+      *params, secret, test_case.id_requirement, GetPartialKeyAccess());
+  ASSERT_THAT(key, IsOk());
+
+  EXPECT_THAT(key->GetKeyBytes(GetPartialKeyAccess()), Eq(secret));
+  EXPECT_THAT(key->GetParameters(), Eq(*params));
+  EXPECT_THAT(key->GetIdRequirement(), Eq(test_case.id_requirement));
+  EXPECT_THAT(key->GetOutputPrefix(), Eq(test_case.output_prefix));
+}
+
+TEST(XAesGcmKeyTest, CreateKeyWithInvalidKeySizeFails) {
+  // Key material must be 32 bytes.
+  RestrictedData invalid_secret = RestrictedData(/*num_random_bytes=*/16);
+
+  util::StatusOr<XAesGcmParameters> params = XAesGcmParameters::Create(
+      XAesGcmParameters::Variant::kTink, kSaltSizeBytes);
+  ASSERT_THAT(params, IsOk());
+
+  EXPECT_THAT(XAesGcmKey::Create(*params, invalid_secret,
+                                 /*id_requirement=*/123, GetPartialKeyAccess())
+                  .status(),
+              StatusIs(absl::StatusCode::kInvalidArgument));
+}
+
+TEST(XAesGcmKeyTest, CreateKeyWithInvalidIdRequirementFails) {
+  RestrictedData secret = RestrictedData(kKeySize);
+
+  util::StatusOr<XAesGcmParameters> params = XAesGcmParameters::Create(
+      XAesGcmParameters::Variant::kNoPrefix, kSaltSizeBytes);
+  ASSERT_THAT(params, IsOk());
+
+  EXPECT_THAT(XAesGcmKey::Create(*params, secret,
+                                 /*id_requirement=*/123, GetPartialKeyAccess())
+                  .status(),
+              StatusIs(absl::StatusCode::kInvalidArgument));
+
+  util::StatusOr<XAesGcmParameters> other_params = XAesGcmParameters::Create(
+      XAesGcmParameters::Variant::kTink, kSaltSizeBytes);
+  ASSERT_THAT(params, IsOk());
+
+  EXPECT_THAT(XAesGcmKey::Create(*other_params, secret,
+                                 /*id_requirement=*/absl::nullopt,
+                                 GetPartialKeyAccess())
+                  .status(),
+              StatusIs(absl::StatusCode::kInvalidArgument));
+}
+
+TEST_P(XAesGcmKeyTest, KeyEquals) {
+  TestCase test_case = GetParam();
+
+  RestrictedData secret = RestrictedData(kKeySize);
+
+  util::StatusOr<XAesGcmParameters> params =
+      XAesGcmParameters::Create(test_case.variant, test_case.salt_size_bytes);
+  ASSERT_THAT(params, IsOk());
+
+  util::StatusOr<XAesGcmKey> key = XAesGcmKey::Create(
+      *params, secret, test_case.id_requirement, GetPartialKeyAccess());
+  ASSERT_THAT(key, IsOk());
+
+  util::StatusOr<XAesGcmKey> other_key = XAesGcmKey::Create(
+      *params, secret, test_case.id_requirement, GetPartialKeyAccess());
+  ASSERT_THAT(other_key, IsOk());
+
+  EXPECT_TRUE(*key == *other_key);
+  EXPECT_TRUE(*other_key == *key);
+  EXPECT_FALSE(*key != *other_key);
+  EXPECT_FALSE(*other_key != *key);
+}
+
+TEST(XAesGcmKeyTest, DifferentVariantNotEqual) {
+  RestrictedData secret = RestrictedData(kKeySize);
+
+  util::StatusOr<XAesGcmParameters> params = XAesGcmParameters::Create(
+      XAesGcmParameters::Variant::kTink, kSaltSizeBytes);
+  ASSERT_THAT(params, IsOk());
+
+  util::StatusOr<XAesGcmKey> key =
+      XAesGcmKey::Create(*params, secret,
+                         /*id_requirement=*/0x01020304, GetPartialKeyAccess());
+  ASSERT_THAT(key, IsOk());
+
+  util::StatusOr<XAesGcmParameters> other_params = XAesGcmParameters::Create(
+      XAesGcmParameters::Variant::kNoPrefix, kSaltSizeBytes);
+  ASSERT_THAT(params, IsOk());
+
+  util::StatusOr<XAesGcmKey> other_key = XAesGcmKey::Create(
+      *other_params, secret,
+      /*id_requirement=*/absl::nullopt, GetPartialKeyAccess());
+  ASSERT_THAT(other_key, IsOk());
+
+  EXPECT_TRUE(*key != *other_key);
+  EXPECT_TRUE(*other_key != *key);
+  EXPECT_FALSE(*key == *other_key);
+  EXPECT_FALSE(*other_key == *key);
+}
+
+TEST(XAesGcmKeyTest, DifferentSecretDataNotEqual) {
+  RestrictedData secret1 = RestrictedData(kKeySize);
+  RestrictedData secret2 = RestrictedData(kKeySize);
+  int id_requirement = 0x01020304;
+
+  util::StatusOr<XAesGcmParameters> params = XAesGcmParameters::Create(
+      XAesGcmParameters::Variant::kTink, kSaltSizeBytes);
+  ASSERT_THAT(params, IsOk());
+
+  util::StatusOr<XAesGcmKey> key = XAesGcmKey::Create(
+      *params, secret1, id_requirement, GetPartialKeyAccess());
+  ASSERT_THAT(key, IsOk());
+
+  util::StatusOr<XAesGcmKey> other_key = XAesGcmKey::Create(
+      *params, secret2, id_requirement, GetPartialKeyAccess());
+  ASSERT_THAT(other_key, IsOk());
+
+  EXPECT_TRUE(*key != *other_key);
+  EXPECT_TRUE(*other_key != *key);
+  EXPECT_FALSE(*key == *other_key);
+  EXPECT_FALSE(*other_key == *key);
+}
+
+TEST(XAesGcmKeyTest, DifferentIdRequirementNotEqual) {
+  RestrictedData secret = RestrictedData(kKeySize);
+
+  util::StatusOr<XAesGcmParameters> params = XAesGcmParameters::Create(
+      XAesGcmParameters::Variant::kTink, kSaltSizeBytes);
+  ASSERT_THAT(params, IsOk());
+
+  util::StatusOr<XAesGcmKey> key =
+      XAesGcmKey::Create(*params, secret,
+                         /*id_requirement=*/0x01020304, GetPartialKeyAccess());
+  ASSERT_THAT(key, IsOk());
+
+  util::StatusOr<XAesGcmKey> other_key =
+      XAesGcmKey::Create(*params, secret,
+                         /*id_requirement=*/0x02030405, GetPartialKeyAccess());
+  ASSERT_THAT(other_key, IsOk());
+
+  EXPECT_TRUE(*key != *other_key);
+  EXPECT_TRUE(*other_key != *key);
+  EXPECT_FALSE(*key == *other_key);
+  EXPECT_FALSE(*other_key == *key);
+}
+
+TEST(XAesGcmKeyTest, DifferentSaltSizeNotEqual) {
+  RestrictedData secret = RestrictedData(kKeySize);
+  int id_requirement = 0x01020304;
+
+  util::StatusOr<XAesGcmParameters> params = XAesGcmParameters::Create(
+      XAesGcmParameters::Variant::kTink, kSaltSizeBytes);
+  ASSERT_THAT(params, IsOk());
+
+  util::StatusOr<XAesGcmKey> key = XAesGcmKey::Create(
+      *params, secret, id_requirement, GetPartialKeyAccess());
+  ASSERT_THAT(key, IsOk());
+
+  util::StatusOr<XAesGcmParameters> other_params = XAesGcmParameters::Create(
+      XAesGcmParameters::Variant::kTink, /*salt_size_bytes=*/8);
+  ASSERT_THAT(other_params, IsOk());
+
+  util::StatusOr<XAesGcmKey> other_key = XAesGcmKey::Create(
+      *other_params, secret, id_requirement, GetPartialKeyAccess());
+  ASSERT_THAT(other_key, IsOk());
+
+  EXPECT_TRUE(*key != *other_key);
+  EXPECT_TRUE(*other_key != *key);
+  EXPECT_FALSE(*key == *other_key);
+  EXPECT_FALSE(*other_key == *key);
+}
+
+}  // namespace
+}  // namespace tink
+}  // namespace crypto

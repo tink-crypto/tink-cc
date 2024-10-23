f9de73ff652448c58b8116cdab664d9d6d259657
Moreno Ambrosin <ambrosin@google.com>
2024-10-23 05:16:05 -0700

    Use remote ccache in tink-cc GCP Ubuntu CMake test
    PiperOrigin-RevId: 688921384
    Change-Id: I747e2b01ff45e3e5e8797cf1fbba9416fad46b43

--- kokoro/gcp_ubuntu/cmake/run_tests.sh
+++ kokoro/gcp_ubuntu/cmake/run_tests.sh
@@ -28,8 +28,14 @@
 #
 set -euo pipefail
 
+IS_KOKORO="true"
+if [[ -z "${KOKORO_ARTIFACTS_DIR:-}" ]]; then
+  readonly IS_KOKORO="false"
+fi
+readonly IS_KOKORO
+
 RUN_COMMAND_ARGS=()
-if [[ -n "${KOKORO_ARTIFACTS_DIR:-}" ]]; then
+if [[ "${IS_KOKORO}" == "true" ]]; then
   readonly TINK_BASE_DIR="$(echo "${KOKORO_ARTIFACTS_DIR}"/git*)"
   cd "${TINK_BASE_DIR}/tink_cc"
   source kokoro/testutils/cc_test_container_images.sh
@@ -42,8 +48,64 @@ if [[ -n "${CONTAINER_IMAGE:-}" ]]; then
   RUN_COMMAND_ARGS+=( -c "${CONTAINER_IMAGE}" )
 fi
 
+EXTRA_CMAKE_ARGS=()
+
+# CMake output directory.
+mkdir -p out
+
+if [[ "${IS_KOKORO}" == "true" ]]; then
+  readonly REMOTE_CACHE_URL="gs://${TINK_REMOTE_CACHE_GCS_BUCKET}/cmake/${TINK_CC_CMAKE_IMAGE_HASH}"
+
+  set -x
+  # Activate the service account for the remote cache.
+  gcloud auth activate-service-account \
+    --key-file="${TINK_REMOTE_CACHE_SERVICE_KEY}"
+  gcloud config set project tink-test-infrastructure
+
+  # Try to use the config cache.
+  if gsutil stat "${REMOTE_CACHE_URL}/config_cache/config_cache.tgz"; then
+    echo "Using config cache: ${REMOTE_CACHE_URL}/config_cache/config_cache.tgz"
+    gsutil cat "${REMOTE_CACHE_URL}/config_cache/config_cache.tgz" \
+      | tar -C out -xzf - --strip-components=1
+  fi
+
+  # Try to use the ccache.
+  if gsutil stat "${REMOTE_CACHE_URL}/ccache/ccache.tgz"; then
+    echo "Using ccache: ${REMOTE_CACHE_URL}/ccache/ccache.tgz"
+    mkdir -p ccache
+    gsutil cat "${REMOTE_CACHE_URL}/ccache/ccache.tgz" \
+      | tar -C ccache -xzf - --strip-components=1
+    # Tell CMake to use CCache.
+    EXTRA_CMAKE_ARGS+=( -DCMAKE_CXX_COMPILER_LAUNCHER=ccache )
+  fi
+  set +x
+fi
+readonly EXTRA_CMAKE_ARGS
+
+cat <<EOF > _build_and_test_tink.sh
+#!/bin/bash
+set -euo pipefail
+
+# TODO: b/369963540 - Remove this once the Docker image is updated.
+apt install ccache
+
+set -x
+
+export CCACHE_DIR="\$(pwd)/ccache"
+export CCACHE_READONLY=1
+
+if [[ -d out ]]; then
+  ./kokoro/testutils/run_cmake_tests.sh -o out . ${EXTRA_CMAKE_ARGS[@]}
+else
+  ./kokoro/testutils/run_cmake_tests.sh . ${EXTRA_CMAKE_ARGS[@]}
+fi
+EOF
+
+chmod +x _build_and_test_tink.sh
+
+readonly RUN_COMMAND_ARGS
 ./kokoro/testutils/run_command.sh "${RUN_COMMAND_ARGS[@]}" \
-  ./kokoro/testutils/run_cmake_tests.sh .
+  ./_build_and_test_tink.sh
 
 ./kokoro/testutils/run_command.sh "${RUN_COMMAND_ARGS[@]}" \
   ./kokoro/testutils/run_cmake_tests.sh "examples"

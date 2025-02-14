afdbba14e253fb77920a8742be6713dded91fc4e
Moreno Ambrosin <ambrosin@google.com>
2025-02-14 03:54:52 -0800

    Add primitive getter for JWT MAC
    PiperOrigin-RevId: 726862771
    Change-Id: I4820c150a8674fd5c1c608ef36ca2b1f70bdb32d

--- tink/jwt/internal/jwt_mac_config_v0.cc
+++ tink/jwt/internal/jwt_mac_config_v0.cc
@@ -16,19 +16,128 @@
 
 #include "tink/jwt/internal/jwt_mac_config_v0.h"
 
+#include <memory>
+#include <string>
+#include <utility>
+
 #include "absl/memory/memory.h"
+#include "absl/status/status.h"
 #include "tink/configuration.h"
 #include "tink/internal/configuration_impl.h"
+#include "tink/internal/internal_insecure_secret_key_access.h"
 #include "tink/jwt/internal/jwt_hmac_key_manager.h"
+#include "tink/jwt/internal/jwt_mac_impl.h"
+#include "tink/jwt/internal/jwt_mac_internal.h"
 #include "tink/jwt/internal/jwt_mac_wrapper.h"
+#include "tink/jwt/jwt_hmac_key.h"
+#include "tink/jwt/jwt_hmac_parameters.h"
+#include "tink/jwt/jwt_hmac_proto_serialization.h"
+#include "tink/mac.h"
+#include "tink/partial_key_access.h"
+#include "tink/subtle/common_enums.h"
+#include "tink/subtle/hmac_boringssl.h"
 #include "tink/util/status.h"
+#include "tink/util/statusor.h"
 
 namespace crypto {
 namespace tink {
 namespace jwt_internal {
+namespace {
+
+struct HashAndTagSize {
+  subtle::HashType hash_type;
+  int tag_size;
+};
+
+util::StatusOr<HashAndTagSize> HashTypeAndTagSize(
+    JwtHmacParameters::Algorithm algorithm) {
+  switch (algorithm) {
+    case JwtHmacParameters::Algorithm::kHs256:
+      return {{subtle::HashType::SHA256, 32}};
+    case JwtHmacParameters::Algorithm::kHs384:
+      return {{subtle::HashType::SHA384, 48}};
+    case JwtHmacParameters::Algorithm::kHs512:
+      return {{subtle::HashType::SHA512, 64}};
+    default:
+      return util::Status(absl::StatusCode::kInvalidArgument,
+                          "Unsupported algorithm");
+  }
+}
+util::StatusOr<std::string> AlgorithmName(
+    JwtHmacParameters::Algorithm algorithm) {
+  switch (algorithm) {
+    case JwtHmacParameters::Algorithm::kHs256:
+      return "HS256";
+    case JwtHmacParameters::Algorithm::kHs384:
+      return "HS384";
+    case JwtHmacParameters::Algorithm::kHs512:
+      return "HS512";
+    default:
+      return util::Status(absl::StatusCode::kInvalidArgument,
+                          "Unsupported algorithm");
+  }
+}
+
+util::StatusOr<std::unique_ptr<JwtMacInternal>> NewJwHmacInternal(
+    const JwtHmacKey& key) {
+  const JwtHmacParameters& jwt_hmac_params = key.GetParameters();
+  util::StatusOr<HashAndTagSize> hash_and_tag_size =
+      HashTypeAndTagSize(jwt_hmac_params.GetAlgorithm());
+  if (!hash_and_tag_size.ok()) {
+    return hash_and_tag_size.status();
+  }
+
+  // Get a raw MAC primitive.
+  util::StatusOr<std::unique_ptr<Mac>> raw_mac = subtle::HmacBoringSsl::New(
+      hash_and_tag_size->hash_type, hash_and_tag_size->tag_size,
+      key.GetKeyBytes(GetPartialKeyAccess())
+          .Get(internal::GetInsecureSecretKeyAccessInternal()));
+  if (!raw_mac.ok()) {
+    return raw_mac.status();
+  }
+
+  util::StatusOr<std::string> algorithm_name =
+      AlgorithmName(jwt_hmac_params.GetAlgorithm());
+  if (!algorithm_name.ok()) {
+    return algorithm_name.status();
+  }
+
+  switch (jwt_hmac_params.GetKidStrategy()) {
+    case JwtHmacParameters::KidStrategy::kIgnored:
+      return JwtMacImpl::Raw(*std::move(raw_mac), *algorithm_name);
+    case JwtHmacParameters::KidStrategy::kCustom: {
+      return JwtMacImpl::RawWithCustomKid(*std::move(raw_mac), *algorithm_name,
+                                          key.GetKid().value());
+    }
+    case JwtHmacParameters::KidStrategy::kBase64EncodedKeyId: {
+      // NOTE: This currently cannot be tested using Tink public APIs: the
+      // keyset wrapper always deals with "RAW" keys.
+      // https://github.com/tink-crypto/tink-cc/blob/ed2008a7b9f09b726a9fff4d96bb9b18093e71c3/tink/internal/keyset_wrapper_impl.h#L83
+      return JwtMacImpl::WithKid(*std::move(raw_mac), *algorithm_name,
+                                 key.GetKid().value());
+    }
+    default:
+      // Should never happen.
+      return util::Status(absl::StatusCode::kInternal,
+                          "Unsupported kid strategy");
+  }
+}
+
+}  // namespace
 
 util::Status AddJwtMacV0(Configuration& config) {
-  util::Status status = internal::ConfigurationImpl::AddPrimitiveWrapper(
+  util::Status status = RegisterJwtHmacProtoSerialization();
+  if (!status.ok()) {
+    return status;
+  }
+  status = internal::ConfigurationImpl::AddPrimitiveGetter<JwtMacInternal,
+                                                           JwtHmacKey>(
+      NewJwHmacInternal, config);
+  if (!status.ok()) {
+    return status;
+  }
+
+  status = internal::ConfigurationImpl::AddPrimitiveWrapper(
       absl::make_unique<JwtMacWrapper>(), config);
   if (!status.ok()) {
     return status;
--- tink/jwt/internal/jwt_mac_config_v0_test.cc
+++ tink/jwt/internal/jwt_mac_config_v0_test.cc
@@ -18,24 +18,36 @@
 
 #include <memory>
 #include <string>
+#include <utility>
+#include <vector>
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "absl/log/check.h"
+#include "absl/strings/escaping.h"
+#include "absl/strings/string_view.h"
 #include "absl/time/clock.h"
 #include "absl/time/time.h"
 #include "tink/configuration.h"
+#include "tink/insecure_secret_key_access.h"
 #include "tink/internal/configuration_impl.h"
 #include "tink/internal/key_gen_configuration_impl.h"
 #include "tink/internal/key_type_info_store.h"
 #include "tink/internal/keyset_wrapper_store.h"
 #include "tink/jwt/internal/jwt_hmac_key_manager.h"
 #include "tink/jwt/internal/jwt_mac_key_gen_config_v0.h"
+#include "tink/jwt/jwt_hmac_key.h"
+#include "tink/jwt/jwt_hmac_parameters.h"
 #include "tink/jwt/jwt_key_templates.h"
 #include "tink/jwt/jwt_mac.h"
 #include "tink/jwt/jwt_validator.h"
 #include "tink/jwt/raw_jwt.h"
+#include "tink/key.h"
 #include "tink/key_gen_configuration.h"
+#include "tink/key_status.h"
 #include "tink/keyset_handle.h"
+#include "tink/partial_key_access.h"
+#include "tink/restricted_data.h"
 #include "tink/util/statusor.h"
 #include "tink/util/test_matchers.h"
 
@@ -116,6 +128,177 @@ TEST_P(JwtMacV0KeyTypesTest, GetPrimitive) {
   ASSERT_THAT(compact, IsOk());
   EXPECT_THAT((*jwt_mac)->VerifyMacAndDecode(*compact, *validator), IsOk());
 }
+
+// From https://datatracker.ietf.org/doc/html/rfc7515#appendix-A.1
+constexpr absl::string_view kKey =
+    "AyM1SysPpbyDfgZld3umj1qzKObwVMkoqQ-EstJQLr_T-"
+    "1qS0gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr1Z9CAow";
+
+struct JwtMacTestParam {
+  std::shared_ptr<const Key> key;
+  std::string signed_jwt;
+  JwtValidator signed_jwt_validator;
+};
+
+std::string Base64WebSafeDecode(absl::string_view base64_string) {
+  std::string dest;
+  CHECK(absl::WebSafeBase64Unescape(base64_string, &dest))
+      << "Failed to base64 decode.";
+
+  return dest;
+}
+
+std::vector<JwtMacTestParam> GetJwtMacTestParams() {
+  std::vector<JwtMacTestParam> res;
+  std::string key_bytes = Base64WebSafeDecode(kKey);
+  {
+    absl::StatusOr<JwtHmacParameters> params = JwtHmacParameters::Create(
+        key_bytes.size(), JwtHmacParameters::KidStrategy::kIgnored,
+        JwtHmacParameters::Algorithm::kHs256);
+    CHECK_OK(params);
+
+    absl::StatusOr<JwtHmacKey> key =
+        JwtHmacKey::Builder()
+            .SetParameters(*params)
+            .SetKeyBytes(
+                RestrictedData(key_bytes, InsecureSecretKeyAccess::Get()))
+            .Build(GetPartialKeyAccess());
+    CHECK_OK(key);
+
+    util::StatusOr<JwtValidator> test_vector_validator =
+        JwtValidatorBuilder()
+            .ExpectIssuer("joe")
+            .ExpectTypeHeader("JWT")
+            .SetFixedNow(absl::FromUnixSeconds(1300819380 - 3600))
+            .Build();
+
+    // From https://datatracker.ietf.org/doc/html/rfc7515#appendix-A.1
+    res.push_back(JwtMacTestParam{
+        /*key=*/std::make_shared<JwtHmacKey>(*key),
+        /*signed_jwt=*/
+        // {"typ":"JWT","alg":"HS256"}
+        "eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9"
+        "."
+        // {"iss":"joe",
+        //  "exp":1300819380,
+        //  "http://example.com/is_root":true}
+        "eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFt"
+        "cGxlLmNvbS9pc19yb290Ijp0cnVlfQ"
+        "."
+        "dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk",
+        /*signed_jwt_validator=*/*std::move(test_vector_validator)});
+  }
+  {
+    absl::StatusOr<JwtHmacParameters> params = JwtHmacParameters::Create(
+        key_bytes.size(), JwtHmacParameters::KidStrategy::kBase64EncodedKeyId,
+        JwtHmacParameters::Algorithm::kHs256);
+    CHECK_OK(params);
+    absl::StatusOr<JwtHmacKey> key =
+        JwtHmacKey::Builder()
+            .SetParameters(*params)
+            .SetIdRequirement(0x01020304)
+            .SetKeyBytes(
+                RestrictedData(key_bytes, InsecureSecretKeyAccess::Get()))
+            .Build(GetPartialKeyAccess());
+    CHECK_OK(key);
+
+    util::StatusOr<JwtValidator> test_vector_validator =
+        JwtValidatorBuilder()
+            .ExpectIssuer("issuer")
+            .AllowMissingExpiration()
+            .Build();
+
+    // Generated.
+    res.push_back(JwtMacTestParam{
+        /*key=*/std::make_shared<JwtHmacKey>(*key),
+        /*signed_jwt=*/
+        // {"kid":"AQIDBA","alg":"HS256"}
+        "eyJraWQiOiJBUUlEQkEiLCJhbGciOiJIUzI1NiJ9"
+        "."
+        // {"iss":"issuer"}
+        "eyJpc3MiOiJpc3N1ZXIifQ"
+        "."
+        "LyeYhbBBMFNjdGo_Qz3SXB7QvYbb-i0Onswr5R7zKvg",
+        /*signed_jwt_validator=*/*std::move(test_vector_validator)});
+  }
+  {
+    absl::StatusOr<JwtHmacParameters> params = JwtHmacParameters::Create(
+        key_bytes.size(), JwtHmacParameters::KidStrategy::kCustom,
+        JwtHmacParameters::Algorithm::kHs256);
+    CHECK_OK(params);
+    absl::StatusOr<JwtHmacKey> key =
+        JwtHmacKey::Builder()
+            .SetParameters(*params)
+            .SetCustomKid("custom-kid")
+            .SetKeyBytes(
+                RestrictedData(key_bytes, InsecureSecretKeyAccess::Get()))
+            .Build(GetPartialKeyAccess());
+    CHECK_OK(key);
+
+    util::StatusOr<JwtValidator> test_vector_validator =
+        JwtValidatorBuilder()
+            .ExpectIssuer("issuer")
+            .AllowMissingExpiration()
+            .Build();
+
+    // Generated.
+    res.push_back(JwtMacTestParam{
+        /*key=*/std::make_shared<JwtHmacKey>(*key),
+        /*signed_jwt=*/
+        // {"kid":"custom-kid","alg":"HS256"}
+        "eyJraWQiOiJjdXN0b20ta2lkIiwiYWxnIjoiSFMyNTYifQ"
+        "."
+        // {"iss":"issuer"}
+        "eyJpc3MiOiJpc3N1ZXIifQ"
+        "."
+        "9t5toIv2qTXGyaKYPKZO_b40dtVWIYj8sPLXzFhNXk0",
+        /*signed_jwt_validator=*/*std::move(test_vector_validator)});
+  }
+  return res;
+}
+
+using JwtMacTest = TestWithParam<JwtMacTestParam>;
+
+TEST_P(JwtMacTest, GetPrimitive) {
+  KeyGenConfiguration key_gen_config;
+  ASSERT_THAT(AddJwtMacKeyGenV0(key_gen_config), IsOk());
+  Configuration config;
+  ASSERT_THAT(AddJwtMacV0(config), IsOk());
+
+  JwtMacTestParam test_vector = GetParam();
+
+  util::StatusOr<KeysetHandle> handle =
+      KeysetHandleBuilder()
+          .AddEntry(KeysetHandleBuilder::Entry::CreateFromKey(
+              test_vector.key, KeyStatus::kEnabled,
+              /*is_primary=*/true))
+          .Build();
+  ASSERT_THAT(handle, IsOk());
+  util::StatusOr<std::unique_ptr<JwtMac>> mac =
+      handle->GetPrimitive<JwtMac>(config);
+  ASSERT_THAT(mac, IsOk());
+
+  // Sign and verify a token.
+  util::StatusOr<RawJwt> raw_jwt =
+      RawJwtBuilder().SetIssuer("issuer").WithoutExpiration().Build();
+  ASSERT_THAT(raw_jwt, IsOk());
+  util::StatusOr<std::string> compact = (*mac)->ComputeMacAndEncode(*raw_jwt);
+  ASSERT_THAT(compact, IsOk());
+  util::StatusOr<JwtValidator> validator = JwtValidatorBuilder()
+                                               .ExpectIssuer("issuer")
+                                               .AllowMissingExpiration()
+                                               .Build();
+  ASSERT_THAT(validator, IsOk());
+  EXPECT_THAT((*mac)->VerifyMacAndDecode(*compact, *validator), IsOk());
+
+  // Verify the test vector signed JWT with the JWT validator.
+  EXPECT_THAT((*mac)->VerifyMacAndDecode(test_vector.signed_jwt,
+                                         test_vector.signed_jwt_validator),
+              IsOk());
+}
+
+INSTANTIATE_TEST_SUITE_P(JwtMacTestSuite, JwtMacTest,
+                         testing::ValuesIn(GetJwtMacTestParams()));
 
 }  // namespace
 }  // namespace jwt_internal

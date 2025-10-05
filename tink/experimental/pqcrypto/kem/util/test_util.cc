dfcca0b2f431a9cbf35216f6e9a8a706256e91d5
Tink Team <tink-dev@google.com>
2025-10-04 21:05:12 -0700

    Automated Code Change
    PiperOrigin-RevId: 815246327
    Change-Id: If8d5cf9212e0fd6c18d128049b4d9f94267697ce

--- tink/experimental/pqcrypto/kem/util/test_util.cc
+++ tink/experimental/pqcrypto/kem/util/test_util.cc
@@ -88,7 +88,8 @@ google::crypto::tink::Cecpq2AeadHkdfPrivateKey GetCecpq2AesGcmHkdfTestKey(
   google::crypto::tink::AesGcmKeyFormat key_format;
   key_format.set_key_size(aes_gcm_key_size);
   auto aead_dem = params->mutable_dem_params()->mutable_aead_dem();
-  std::unique_ptr<AesGcmKeyManager> key_manager(new AesGcmKeyManager());
+  std::unique_ptr<AesGcmKeyManager> key_manager =
+      std::make_unique<AesGcmKeyManager>();
   std::string dem_key_type = key_manager->get_key_type();
   aead_dem->set_type_url(dem_key_type);
   aead_dem->set_value(key_format.SerializeAsString());
@@ -119,8 +120,8 @@ google::crypto::tink::Cecpq2AeadHkdfPrivateKey GetCecpq2AesCtrHmacHkdfTestKey(
   auto params = ecies_key.mutable_public_key()->mutable_params();
   auto aead_dem = params->mutable_dem_params()->mutable_aead_dem();
 
-  std::unique_ptr<AesCtrHmacAeadKeyManager> key_manager(
-      new AesCtrHmacAeadKeyManager());
+  std::unique_ptr<AesCtrHmacAeadKeyManager> key_manager =
+      std::make_unique<AesCtrHmacAeadKeyManager>();
   std::string dem_key_type = key_manager->get_key_type();
   aead_dem->set_type_url(dem_key_type);
   aead_dem->set_value(key_format.SerializeAsString());
@@ -138,8 +139,8 @@ GetCecpq2XChaCha20Poly1305HkdfTestKey(
 
   google::crypto::tink::XChaCha20Poly1305KeyFormat key_format;
   auto aead_dem = params->mutable_dem_params()->mutable_aead_dem();
-  std::unique_ptr<XChaCha20Poly1305KeyManager> key_manager(
-      new XChaCha20Poly1305KeyManager());
+  std::unique_ptr<XChaCha20Poly1305KeyManager> key_manager =
+      std::make_unique<XChaCha20Poly1305KeyManager>();
   std::string dem_key_type = key_manager->get_key_type();
   aead_dem->set_type_url(dem_key_type);
   aead_dem->set_value(key_format.SerializeAsString());

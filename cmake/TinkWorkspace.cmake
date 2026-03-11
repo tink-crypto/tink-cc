67aa27846aae690c40f239dc84522b061fe0f868
Charles Lee <ckl@google.com>
2026-03-11 13:40:15 -0700

    Remove CMake directory alias for v0 test vectors.
    PiperOrigin-RevId: 882174431
    Change-Id: I789c74b826d854c1876f91448d307a1398926879

--- cmake/TinkWorkspace.cmake
+++ cmake/TinkWorkspace.cmake
@@ -81,9 +81,7 @@ if (TINK_BUILD_TESTS)
     DATA_ONLY
   )
   # Symlink the Wycheproof test data.
-  # Tests expect Wycheproof test vectors to be in a local testvectors{,_v1}/ folder.
-  add_directory_alias("${wycheproof_SOURCE_DIR}/testvectors"
-    "${CMAKE_BINARY_DIR}/testvectors")
+  # Tests expect Wycheproof test vectors to be in a local testvectors_v1/ folder.
   add_directory_alias("${wycheproof_SOURCE_DIR}/testvectors_v1"
     "${CMAKE_BINARY_DIR}/testvectors_v1")
 

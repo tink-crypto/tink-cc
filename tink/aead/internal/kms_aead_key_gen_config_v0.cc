e9e0594a80e936f2140e0264ef5dd420ee06bd9a
Jérémy Prudhommeaux <jprudhommeaux@google.com>
2026-05-19 05:03:21 -0700

    Add internal V0 configuration for KMS AEAD.
    PiperOrigin-RevId: 917768187
    Change-Id: I0493f2c94b3d7a9eaca7ca4cb83a6cbd393ffa64

---
+++ tink/aead/internal/kms_aead_key_gen_config_v0.cc
@@ -0,0 +1,42 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "tink/aead/internal/kms_aead_key_gen_config_v0.h"
+
+#include "absl/memory/memory.h"
+#include "absl/status/status.h"
+#include "tink/aead/kms_aead_key_manager.h"
+#include "tink/aead/kms_envelope_aead_key_manager.h"
+#include "tink/internal/key_gen_configuration_impl.h"
+#include "tink/key_gen_configuration.h"
+
+namespace crypto {
+namespace tink {
+namespace internal {
+
+absl::Status AddKmsAeadKeyGenV0(KeyGenConfiguration& config) {
+  absl::Status status = KeyGenConfigurationImpl::AddKeyTypeManager(
+      absl::make_unique<KmsAeadKeyManager>(), config);
+  if (!status.ok()) {
+    return status;
+  }
+  return KeyGenConfigurationImpl::AddKeyTypeManager(
+      absl::make_unique<KmsEnvelopeAeadKeyManager>(), config);
+}
+
+}  // namespace internal
+}  // namespace tink
+}  // namespace crypto

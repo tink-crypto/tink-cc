f2ab4b050be0e83d809a6755b9de992dc865b62b
Jérémy Prudhommeaux <jprudhommeaux@google.com>
2026-05-29 04:04:37 -0700

    Add public 2026 configuration for key derivation primitive.
    PiperOrigin-RevId: 923317663
    Change-Id: I4361763392d157727f2e24a8529386103816793a

---
+++ tink/keyderivation/config_2026.h
@@ -0,0 +1,32 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef TINK_KEYDERIVATION_CONFIG_2026_H_
+#define TINK_KEYDERIVATION_CONFIG_2026_H_
+
+#include "tink/configuration.h"
+
+namespace crypto {
+namespace tink {
+
+// Configuration used to generate KeyDerivation primitives with 2026 recommended
+// key managers.
+const Configuration& ConfigKeyDerivation2026();
+
+}  // namespace tink
+}  // namespace crypto
+
+#endif  // TINK_KEYDERIVATION_CONFIG_2026_H_
---
+++ tink/keyderivation/key_gen_config_2026.cc
@@ -0,0 +1,36 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "tink/keyderivation/key_gen_config_2026.h"
+
+#include "absl/log/absl_check.h"
+#include "tink/key_gen_configuration.h"
+#include "tink/keyderivation/internal/key_gen_config_2026.h"
+
+namespace crypto {
+namespace tink {
+
+const KeyGenConfiguration& KeyGenConfigKeyDerivation2026() {
+  static const KeyGenConfiguration* instance = [] {
+    static KeyGenConfiguration* config = new KeyGenConfiguration();
+    ABSL_CHECK_OK(internal::AddKeyDerivationKeyGen2026(*config));
+    return config;
+  }();
+  return *instance;
+}
+
+}  // namespace tink
+}  // namespace crypto

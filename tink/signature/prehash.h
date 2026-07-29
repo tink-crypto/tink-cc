3dcabf9666f136d0fef0c937d6899af905136733
Liza Tretyakova <lizatretyakova@google.com>
2026-07-29 07:25:20 -0700

    Add `Prehash` and `SignPrehash` APIs.
    PiperOrigin-RevId: 955870077
    Change-Id: I59bc64d9e62a2b5639e442d31229253beddb7b1c

---
+++ tink/signature/prehash.h
@@ -0,0 +1,41 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef TINK_SIGNATURE_PREHASH_H_
+#define TINK_SIGNATURE_PREHASH_H_
+
+#include <string>
+
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+
+namespace crypto {
+namespace tink {
+
+// Interface for generating precomputed hash values.
+class Prehash {
+ public:
+  // Computes the digest of `data`. The hash output is prepended with
+  // `0xff | key_id` (5 bytes).
+  virtual absl::StatusOr<std::string> Compute(absl::string_view data) const = 0;
+
+  virtual ~Prehash();
+};
+
+}  // namespace tink
+}  // namespace crypto
+
+#endif  // TINK_SIGNATURE_PREHASH_H_

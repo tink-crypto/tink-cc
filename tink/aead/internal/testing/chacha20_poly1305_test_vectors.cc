d9bce2c581370d168022d540a5268903eeaff84c
Jérémy Prudhommeaux <jprudhommeaux@google.com>
2026-08-08 11:06:58 -0700

    Optimize aead/internal/testing/chacha20_poly1305_test_vectors by introducing static test vectors
    PiperOrigin-RevId: 961463980
    Change-Id: I4669b13e447159e6260d6713536044d1f9805bc6

---
+++ tink/aead/internal/testing/chacha20_poly1305_test_vectors.cc
@@ -0,0 +1,149 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "tink/aead/internal/testing/chacha20_poly1305_test_vectors.h"
+
+#include <memory>
+#include <optional>
+#include <utility>
+#include <vector>
+
+#include "absl/base/no_destructor.h"
+#include "absl/container/flat_hash_map.h"
+#include "absl/log/absl_check.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+#include "absl/types/optional.h"
+#include "tink/aead/chacha20_poly1305_key.h"
+#include "tink/aead/chacha20_poly1305_parameters.h"
+#include "tink/aead/internal/testing/aead_test_vector.h"
+#include "tink/insecure_secret_key_access.h"
+#include "tink/partial_key_access.h"
+#include "tink/restricted_data.h"
+#include "tink/util/test_util.h"
+
+namespace crypto::tink::internal {
+namespace {
+
+using ::crypto::tink::test::HexDecodeOrDie;
+using ChaCha20Poly1305TestVectorMap =
+    absl::flat_hash_map<ChaCha20Poly1305Parameters::Variant, AeadTestVector>;
+
+struct ChaCha20Poly1305TestVectorParams {
+  ChaCha20Poly1305Parameters::Variant variant;
+  absl::string_view key_hex;
+  std::optional<int> id_requirement;
+  absl::string_view plaintext_hex;
+  absl::string_view associated_data_hex;
+  absl::string_view ciphertext_hex;
+};
+
+AeadTestVector MakeChaCha20Poly1305TestVector(
+    const ChaCha20Poly1305TestVectorParams& params) {
+  absl::StatusOr<ChaCha20Poly1305Parameters> parameters =
+      ChaCha20Poly1305Parameters::Create(params.variant);
+  ABSL_CHECK_OK(parameters.status());
+  absl::StatusOr<ChaCha20Poly1305Key> key = ChaCha20Poly1305Key::Create(
+      params.variant,
+      RestrictedData(HexDecodeOrDie(params.key_hex),
+                     InsecureSecretKeyAccess::Get()),
+      params.id_requirement, GetPartialKeyAccess());
+  ABSL_CHECK_OK(key.status());
+  return AeadTestVector(std::make_shared<ChaCha20Poly1305Key>(*key),
+                        HexDecodeOrDie(params.plaintext_hex),
+                        HexDecodeOrDie(params.associated_data_hex),
+                        HexDecodeOrDie(params.ciphertext_hex));
+}
+
+const ChaCha20Poly1305TestVectorMap& CreateChaCha20Poly1305TestVectorsMap() {
+  static const absl::NoDestructor<ChaCha20Poly1305TestVectorMap> test_vectors(
+      ChaCha20Poly1305TestVectorMap{
+          {ChaCha20Poly1305Parameters::Variant::kNoPrefix,
+           MakeChaCha20Poly1305TestVector(ChaCha20Poly1305TestVectorParams{
+               /*variant=*/ChaCha20Poly1305Parameters::Variant::kNoPrefix,
+               /*key_hex=*/
+               "1c9240a5eb55d38af333888604f6b5f0473917c1402b80099dca5cb"
+               "cce17f83a",
+               /*id_requirement=*/std::nullopt,
+               /*plaintext_hex=*/
+               "f2d814f8a608f824132b772081d80c103fe4f641f21e3a1bf9b2a5d3f4c6"
+               "f7e0",
+               /*associated_data_hex=*/"",
+               /*ciphertext_hex=*/
+               "0000000000000000000000006f208ef1310657962c64b4b88a8d5e8964e"
+               "505f56b68a8b1cb3eb086208a183592185ef8f0b721",
+           })},
+          {ChaCha20Poly1305Parameters::Variant::kTink,
+           MakeChaCha20Poly1305TestVector(ChaCha20Poly1305TestVectorParams{
+               /*variant=*/ChaCha20Poly1305Parameters::Variant::kTink,
+               /*key_hex=*/
+               "1c9240a5eb55d38af333888604f6b5f0473917c1402b80099dca5cb"
+               "cce17f83a",
+               /*id_requirement=*/0x01020304,
+               /*plaintext_hex=*/
+               "f2d814f8a608f824132b772081d80c103fe4f641f21e3a1bf9b2a5d3f4c6"
+               "f7e0",
+               /*associated_data_hex=*/"",
+               /*ciphertext_hex=*/
+               "01010203040000000000000000000000006f208ef1310657962c64b4b88"
+               "a8d5e8964e505f56b68a8b1cb3eb086208a183592185ef8f0b721",
+           })},
+          {ChaCha20Poly1305Parameters::Variant::kCrunchy,
+           MakeChaCha20Poly1305TestVector(ChaCha20Poly1305TestVectorParams{
+               /*variant=*/ChaCha20Poly1305Parameters::Variant::kCrunchy,
+               /*key_hex=*/
+               "1c9240a5eb55d38af333888604f6b5f0473917c1402b80099dca5cb"
+               "cce17f83a",
+               /*id_requirement=*/0x01020304,
+               /*plaintext_hex=*/
+               "f2d814f8a608f824132b772081d80c103fe4f641f21e3a1bf9b2a5d3f4c6"
+               "f7e0",
+               /*associated_data_hex=*/"",
+               /*ciphertext_hex=*/
+               "00010203040000000000000000000000006f208ef1310657962c64b4b88"
+               "a8d5e8964e505f56b68a8b1cb3eb086208a183592185ef8f0b721",
+           })},
+      });
+  return *test_vectors;
+}
+
+}  // namespace
+
+const std::vector<AeadTestVector>& CreateChaCha20Poly1305TestVectors() {
+  static const absl::NoDestructor<std::vector<AeadTestVector>> test_vectors([] {
+    std::vector<AeadTestVector> result;
+    result.reserve(CreateChaCha20Poly1305TestVectorsMap().size());
+    for (const auto& [unused_params, test_vector] :
+         CreateChaCha20Poly1305TestVectorsMap()) {
+      result.push_back(test_vector);
+    }
+    return result;
+  }());
+  return *test_vectors;
+}
+
+const AeadTestVector& GetChaCha20Poly1305TestVector(
+    ChaCha20Poly1305Parameters::Variant variant) {
+  const ChaCha20Poly1305TestVectorMap& test_vectors_map =
+      CreateChaCha20Poly1305TestVectorsMap();
+  auto it = test_vectors_map.find(variant);
+  ABSL_CHECK(it != test_vectors_map.end())
+      << "AeadTestVector not found for ChaCha20-Poly1305 key with variant "
+      << static_cast<int>(variant);
+  return it->second;
+}
+
+}  // namespace crypto::tink::internal
---
+++ tink/aead/internal/testing/chacha20_poly1305_test_vectors.h
@@ -0,0 +1,38 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef TINK_AEAD_INTERNAL_TESTING_CHACHA20_POLY1305_TEST_VECTORS_H_
+#define TINK_AEAD_INTERNAL_TESTING_CHACHA20_POLY1305_TEST_VECTORS_H_
+
+#include <vector>
+
+#include "tink/aead/chacha20_poly1305_parameters.h"
+#include "tink/aead/internal/testing/aead_test_vector.h"
+
+namespace crypto::tink::internal {
+
+// Returns static test vectors for ChaCha20-Poly1305 from Wycheproof
+// (wycheproof/testvectors/chacha20_poly1305_test.json).
+const std::vector<AeadTestVector>& CreateChaCha20Poly1305TestVectors();
+
+// Returns static test vector for ChaCha20-Poly1305 for the given variant from
+// Wycheproof (wycheproof/testvectors/chacha20_poly1305_test.json).
+const AeadTestVector& GetChaCha20Poly1305TestVector(
+    ChaCha20Poly1305Parameters::Variant variant);
+
+}  // namespace crypto::tink::internal
+
+#endif  // TINK_AEAD_INTERNAL_TESTING_CHACHA20_POLY1305_TEST_VECTORS_H_

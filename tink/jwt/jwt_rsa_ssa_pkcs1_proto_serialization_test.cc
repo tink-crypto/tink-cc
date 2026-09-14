7b9d9f30d3cf67398aff5214a4b8b7591ee68f3e
Tink Team <tink-dev@google.com>
2026-09-14 03:48:19 -0700

    Automated Code Change
    PiperOrigin-RevId: 981057997
    Change-Id: Ib9122ae2d7f0e0c4b75d19e3f24f3993ea83bf00

--- tink/jwt/jwt_rsa_ssa_pkcs1_proto_serialization_test.cc
+++ tink/jwt/jwt_rsa_ssa_pkcs1_proto_serialization_test.cc
@@ -290,12 +290,13 @@ TEST_P(JwtRsaSsaPkcs1ProtoSerializationTest, SerializeParametersSucceeds) {
           serialization->get());
 
   ASSERT_THAT(proto_serialization, NotNull());
-  const internal::KeyTemplateTP& key_template =
+  const google::crypto::tink::internal::KeyTemplateTP& key_template =
       proto_serialization->GetKeyTemplate();
   EXPECT_THAT(key_template.type_url(), Eq(kPrivateTypeUrl));
-  EXPECT_THAT(key_template.output_prefix_type(),
-              Eq(static_cast<internal::OutputPrefixTypeTP>(
-                  test_case.output_prefix_type)));
+  EXPECT_THAT(
+      key_template.output_prefix_type(),
+      Eq(static_cast<google::crypto::tink::internal::OutputPrefixTypeTP>(
+          test_case.output_prefix_type)));
   JwtRsaSsaPkcs1KeyFormat key_format;
   ASSERT_THAT(key_format.ParseFromString(key_template.value()), IsTrue());
 

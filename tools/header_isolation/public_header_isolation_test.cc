658205e6b08c3dde00685bb1c67ea0d4dda2b5e2
Jérémy Prudhommeaux <jprudhommeaux@google.com>
2026-09-16 01:52:25 -0700

    Add single-key creation API for RsaSsaPkcs1 to subtle directory .
    PiperOrigin-RevId: 982361153
    Change-Id: Id158ffd5944c60c13986e1abd00426e97e8bce54

--- tools/header_isolation/public_header_isolation_test.cc
+++ tools/header_isolation/public_header_isolation_test.cc
@@ -369,6 +369,7 @@
 #include "tink/signature/subtle/composite_ml_dsa_verify_boringssl.h"
 #include "tink/signature/subtle/create_ecdsa_key.h"
 #include "tink/signature/subtle/create_ed25519_key.h"
+#include "tink/signature/subtle/create_rsa_ssa_pkcs1_key.h"
 #include "tink/signature_config.h"
 #include "tink/signature_key_templates.h"
 #include "tink/streaming_aead.h"

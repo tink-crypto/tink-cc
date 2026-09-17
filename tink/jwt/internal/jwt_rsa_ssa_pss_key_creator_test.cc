00236809c7dee2f808b5569f01aa6fbdf374cf12
Jérémy Prudhommeaux <jprudhommeaux@google.com>
2026-09-17 02:46:36 -0700

    Add internal key creator for JwtRsaSsaPss.
    PiperOrigin-RevId: 983068800
    Change-Id: I3576e980a7b642cc232738d7d48a76a15d014eee

---
+++ tink/jwt/internal/jwt_rsa_ssa_pss_key_creator_test.cc
@@ -0,0 +1,144 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "tink/jwt/internal/jwt_rsa_ssa_pss_key_creator.h"
+
+#include <optional>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "absl/status/status.h"
+#include "absl/status/status_matchers.h"
+#include "absl/status/statusor.h"
+#include "tink/jwt/jwt_rsa_ssa_pss_parameters.h"
+#include "tink/jwt/jwt_rsa_ssa_pss_private_key.h"
+
+namespace crypto {
+namespace tink {
+namespace internal {
+namespace {
+
+using ::absl_testing::IsOk;
+using ::absl_testing::StatusIs;
+using ::testing::Eq;
+using ::testing::IsFalse;
+
+TEST(JwtRsaSsaPssKeyCreatorTest,
+     CreatePrivateJwtRsaSsaPssKeyWorksBase64EncodedKeyId) {
+  absl::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs256)
+          .SetKidStrategy(
+              JwtRsaSsaPssParameters::KidStrategy::kBase64EncodedKeyId)
+          .SetModulusSizeInBits(2048)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  absl::StatusOr<JwtRsaSsaPssPrivateKey> key =
+      CreatePrivateJwtRsaSsaPssKey(*parameters, /*id_requirement=*/123);
+  ASSERT_THAT(key, IsOk());
+  EXPECT_THAT(key->GetParameters(), Eq(*parameters));
+  EXPECT_THAT(key->GetIdRequirement(), Eq(123));
+}
+
+TEST(JwtRsaSsaPssKeyCreatorTest, CreatePrivateJwtRsaSsaPssKeyWorksIgnored) {
+  absl::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs384)
+          .SetKidStrategy(JwtRsaSsaPssParameters::KidStrategy::kIgnored)
+          .SetModulusSizeInBits(2048)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  absl::StatusOr<JwtRsaSsaPssPrivateKey> key =
+      CreatePrivateJwtRsaSsaPssKey(*parameters);
+  ASSERT_THAT(key, IsOk());
+  EXPECT_THAT(key->GetParameters(), Eq(*parameters));
+  EXPECT_THAT(key->GetIdRequirement().has_value(), IsFalse());
+}
+
+TEST(JwtRsaSsaPssKeyCreatorTest, CreatePrivateJwtRsaSsaPssKeyWorksCustomKid) {
+  absl::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs512)
+          .SetKidStrategy(JwtRsaSsaPssParameters::KidStrategy::kCustom)
+          .SetModulusSizeInBits(2048)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  absl::StatusOr<JwtRsaSsaPssPrivateKey> key =
+      CreatePrivateJwtRsaSsaPssKey(*parameters, /*id_requirement=*/std::nullopt,
+                                   /*custom_kid=*/"my_custom_kid");
+  ASSERT_THAT(key, IsOk());
+  EXPECT_THAT(key->GetParameters(), Eq(*parameters));
+  EXPECT_THAT(key->GetPublicKey().GetKid(), Eq("my_custom_kid"));
+}
+
+TEST(JwtRsaSsaPssKeyCreatorTest, MissingIdRequirementForBase64KeyIdFails) {
+  absl::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs256)
+          .SetKidStrategy(
+              JwtRsaSsaPssParameters::KidStrategy::kBase64EncodedKeyId)
+          .SetModulusSizeInBits(2048)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  EXPECT_THAT(CreatePrivateJwtRsaSsaPssKey(*parameters,
+                                           /*id_requirement=*/std::nullopt),
+              StatusIs(absl::StatusCode::kInvalidArgument));
+}
+
+TEST(JwtRsaSsaPssKeyCreatorTest, MissingCustomKidForCustomKidStrategyFails) {
+  absl::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs256)
+          .SetKidStrategy(JwtRsaSsaPssParameters::KidStrategy::kCustom)
+          .SetModulusSizeInBits(2048)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  EXPECT_THAT(CreatePrivateJwtRsaSsaPssKey(*parameters,
+                                           /*id_requirement=*/std::nullopt,
+                                           /*custom_kid=*/std::nullopt),
+              StatusIs(absl::StatusCode::kInvalidArgument));
+}
+
+TEST(JwtRsaSsaPssKeyCreatorTest, SuccessiveKeysAreDistinct) {
+  absl::StatusOr<JwtRsaSsaPssParameters> parameters =
+      JwtRsaSsaPssParameters::Builder()
+          .SetAlgorithm(JwtRsaSsaPssParameters::Algorithm::kPs256)
+          .SetKidStrategy(
+              JwtRsaSsaPssParameters::KidStrategy::kBase64EncodedKeyId)
+          .SetModulusSizeInBits(2048)
+          .Build();
+  ASSERT_THAT(parameters, IsOk());
+
+  absl::StatusOr<JwtRsaSsaPssPrivateKey> key1 =
+      CreatePrivateJwtRsaSsaPssKey(*parameters, /*id_requirement=*/123);
+  ASSERT_THAT(key1, IsOk());
+
+  absl::StatusOr<JwtRsaSsaPssPrivateKey> key2 =
+      CreatePrivateJwtRsaSsaPssKey(*parameters, /*id_requirement=*/123);
+  ASSERT_THAT(key2, IsOk());
+
+  EXPECT_NE(*key1, *key2);
+}
+
+}  // namespace
+}  // namespace internal
+}  // namespace tink
+}  // namespace crypto

a8a15ef40e5b324f48b6545eedcbbaf6de9a476c
Jérémy Prudhommeaux <jprudhommeaux@google.com>
2026-09-17 06:16:09 -0700

    Add single-key creation API for JwtRsaSsaPss to subtle directory .
    PiperOrigin-RevId: 983151106
    Change-Id: Ie72c9692ad6a659561edb7125b9e2f8fb0dce2cb

---
+++ tink/jwt/subtle/create_jwt_rsa_ssa_pss_key.h
@@ -0,0 +1,44 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef TINK_JWT_SUBTLE_CREATE_JWT_RSA_SSA_PSS_KEY_H_
+#define TINK_JWT_SUBTLE_CREATE_JWT_RSA_SSA_PSS_KEY_H_
+
+#include <optional>
+#include <string_view>
+
+#include "absl/status/statusor.h"
+#include "tink/jwt/jwt_rsa_ssa_pss_parameters.h"
+#include "tink/jwt/jwt_rsa_ssa_pss_private_key.h"
+
+namespace crypto {
+namespace tink {
+namespace subtle {
+
+// Most users should not call this API directly. Instead, most users should
+// utilize keysets to ensure key rotation.
+//
+// Generates a new standalone JwtRsaSsaPssPrivateKey from parameters.
+absl::StatusOr<JwtRsaSsaPssPrivateKey> CreateJwtRsaSsaPssPrivateKey(
+    const JwtRsaSsaPssParameters& parameters,
+    std::optional<int> id_requirement = std::nullopt,
+    std::optional<std::string_view> custom_kid = std::nullopt);
+
+}  // namespace subtle
+}  // namespace tink
+}  // namespace crypto
+
+#endif  // TINK_JWT_SUBTLE_CREATE_JWT_RSA_SSA_PSS_KEY_H_
